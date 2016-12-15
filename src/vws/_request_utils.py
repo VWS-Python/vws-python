094f199c55f05609595a7d60b9cb1de0819229ac
Adam Dangoor <adamdangoor@gmail.com>
2016-12-15 02:34:37 +0000

    Progress towards header

--- src/vws/_request_utils.py
+++ src/vws/_request_utils.py
@@ -3,3 +3,42 @@ Utilities for making requests to Vuforia.
 
 Based on Python examples from https://developer.vuforia.com/downloads/samples.
 """
+
+import hashlib
+import hmac
+import base64
+
+
+def compute_md5_hex(data):
+    """Return the hex MD5 of the data"""
+    h = hashlib.md5()
+    h.update(data)
+    return h.hexdigest()
+
+
+def compute_hmac_base64(key: bytes, data) -> bytes:
+    """Return the Base64 encoded HMAC-SHA1 using the provide key"""
+    h = hmac.new(key=key, msg=None, digestmod=hashlib.sha1)
+    h.update(msg=data)
+    return base64.b64encode(s=h.digest())
+
+
+def authorization_header_for_request(access_key: str, secret_key: str,
+                                     method: str, content: bytes,
+                                     content_type: str, date: str,
+                                     request_path: str) -> bytes:
+    """Return the value of the Authorization header for the request parameters
+
+    TODO Args, Return
+    """
+    components_to_sign = list()
+    components_to_sign.append(method)
+    components_to_sign.append(str(compute_md5_hex(content)))
+    components_to_sign.append(str(content_type))
+    components_to_sign.append(str(date))
+    components_to_sign.append(str(request_path))
+    string_to_sign = "\n".join(components_to_sign)
+    string_to_sign = bytes(string_to_sign, encoding='ascii')
+    signature = compute_hmac_base64(key=secret_key, data=string_to_sign)
+    auth_header = "VWS %s:%s" % (access_key, signature)
+    return auth_header
--- tests/test_request_utils.py
+++ tests/test_request_utils.py
@@ -1,3 +1,34 @@
 """
 Tests for `vws._request_utils`.
 """
+
+from vws._request_utils import authorization_header_for_request
+
+
+class TestAuthorizationHeaderForRequest:
+    """
+        TODO
+        """
+
+    def test_example(self):
+        expected = (
+            "X"
+        )
+
+        access_key = 'a'
+        secret_key = b'b'
+        method = 'c'
+        content = b'content'
+        content_type = 'application/json'
+        date = 'date'
+        request_path = 'rp'
+
+        assert authorization_header_for_request(
+            access_key=access_key,
+            secret_key=secret_key,
+            method=method,
+            content=content,
+            content_type=content_type,
+            date=date,
+            request_path=request_path,
+        ) == expected
