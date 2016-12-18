5872458db5766153006aa01b1a983dcd0a5bc7f8
Adam Dangoor <adamdangoor@gmail.com>
2016-12-18 13:40:15 +0000

    Remove complexity from fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,17 +31,8 @@ def vuforia_server_credentials() -> VuforiaServerCredentials:
     """
     Return VWS credentials from environment variables.
     """
-    vuforia_server_access_key = os.getenv('VUFORIA_SERVER_ACCESS_KEY')
-    vuforia_server_secret_key = os.getenv('VUFORIA_SERVER_SECRET_KEY')
-
-    if not all([vuforia_server_access_key, vuforia_server_secret_key]):
-        pytest.skip(
-            "Vuforia integration tests require secrets to be set in "
-            "environment variables."
-        )
-
     credentials = VuforiaServerCredentials(
-        access_key=vuforia_server_access_key,
-        secret_key=vuforia_server_secret_key,
+        access_key=os.environ['VUFORIA_SERVER_ACCESS_KEY'],
+        secret_key=os.environ['VUFORIA_SERVER_SECRET_KEY'],
     )
     return credentials
