7d114d6035a05fbb53430348cf22af631f55583d
Adam Dangoor <adamdangoor@gmail.com>
2016-12-20 17:02:44 +0000

    Remove autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,7 @@ import sphinx_rtd_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['sphinx.ext.autodoc']
-autodoc_default_flags = ['members']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
