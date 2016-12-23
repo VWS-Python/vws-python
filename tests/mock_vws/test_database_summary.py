73eb64f2fe230146d6a74f7bbf9860bc22e12ffb
Adam Dangoor <adamdangoor@gmail.com>
2016-12-23 17:35:49 +0000

    More parametrization

--- tests/mock_vws/test_database_summary.py
+++ tests/mock_vws/test_database_summary.py
@@ -100,71 +100,26 @@ class TestDateHeader:
         assert response.json()['result_code'] == 'Fail'
 
     @pytest.mark.parametrize(
-        'time_difference_from_now',
+        ['time_difference_from_now', 'expected_status', 'expected_result'],
         [
-            timedelta(minutes=5, seconds=10),
-            -timedelta(minutes=5, seconds=10),
+            (timedelta(minutes=4, seconds=50), codes.OK, 'Success'),
+            (-timedelta(minutes=4, seconds=50), codes.OK, 'Success'),
+            (timedelta(minutes=5, seconds=10), codes.FORBIDDEN, 'RequestTimeTooSkewed'),
+            (-timedelta(minutes=5, seconds=10), codes.FORBIDDEN, 'RequestTimeTooSkewed'),
         ],
-        ids=(['After Now', 'Before Now'])
-    )
-    def test_date_out_of_range(self,
-                               vuforia_server_credentials:
-                               VuforiaServerCredentials,
-                               time_difference_from_now,
-                               ) -> None:
-        """
-        If the date header is set to more than five minutes before the request
-        is made, or more than five minutes after the request is made an error
-        is returned.
-
-        Because there is a small delay in sending requests and Vuforia isn't
-        consistent, some leeway is given.
-        """
-        with freeze_time(datetime.now() + time_difference_from_now):
-            date = rfc_1123_date()
-
-        content_type = 'application/json'
-
-        signature_string = authorization_header(
-            access_key=vuforia_server_credentials.access_key,
-            secret_key=vuforia_server_credentials.secret_key,
-            method=GET,
-            content=b'',
-            content_type=content_type,
-            date=date,
-            request_path='/summary',
-        )
-
-        headers = {
-            "Authorization": signature_string,
-            "Date": date,
-            "Content-Type": content_type,
-        }
-
-        response = requests.request(
-            method=GET,
-            url='https://vws.vuforia.com/summary',
-            headers=headers,
-            data=b'',
-        )
-
-        assert response.status_code == codes.FORBIDDEN
-        assert response.json().keys() == {'transaction_id', 'result_code'}
-        assert is_valid_transaction_id(response.json()['transaction_id'])
-        assert response.json()['result_code'] == 'RequestTimeTooSkewed'
-
-    @pytest.mark.parametrize(
-        'time_difference_from_now',
-        [
-            timedelta(minutes=4, seconds=50),
-            -timedelta(minutes=4, seconds=50),
-        ],
-        ids=(['After Now', 'Before Now'])
+        ids=([
+            'Within Range After',
+            'Within Range Before',
+            'Out of Range After',
+            'Out of Range Before',
+        ])
     )
     def test_date_within_range(self,
                                vuforia_server_credentials:
                                VuforiaServerCredentials,
                                time_difference_from_now,
+                               expected_status,
+                               expected_result,
                                ) -> None:
         """
         If a date header is within five minutes before or after the request
@@ -201,6 +156,6 @@ class TestDateHeader:
             data=b'',
         )
 
-        assert response.status_code == codes.OK
+        assert response.status_code == expected_status
         assert is_valid_transaction_id(response.json()['transaction_id'])
-        assert response.json()['result_code'] == 'Success'
+        assert response.json()['result_code'] == expected_result
