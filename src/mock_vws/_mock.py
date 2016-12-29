7a9c296411cedb2b2e9218931875d4c74f4807ab
Adam Dangoor <adamdangoor@gmail.com>
2016-12-29 02:26:11 +0000

    Document new attribute

--- src/mock_vws/_mock.py
+++ src/mock_vws/_mock.py
@@ -192,6 +192,7 @@ class MockVuforiaTargetAPI:  # pylint: disable=no-self-use
         Attributes:
             access_key: A VWS access key.
             secret_key: A VWS secret key.
+            routes: The `Route`s to be used in the mock.
         """
         self.access_key = access_key  # type: str
         self.secret_key = secret_key  # type: str
