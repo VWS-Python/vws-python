01e0b6973a827e3f68d63ec06998264936e366e5
Adam Dangoor <adamdangoor@gmail.com>
2016-12-30 15:51:53 +0000

    Add helper files

---
+++ tests/mock_vws/conftest.py
@@ -0,0 +1,27 @@
+
+"""
+Configuration, plugins and fixtures for `pytest`.
+"""
+
+from typing import Generator
+
+import pytest
+from _pytest.fixtures import SubRequest
+
+from mock_vws import MockVWS
+
+
+@pytest.fixture(params=[True, False], ids=['Real Vuforia', 'Mock Vuforia'])
+def verify_mock_vuforia(request: SubRequest) -> Generator:
+    """
+    Using this fixture in a test will make it run twice. Once with the real
+    Vuforia, and once with the mock.
+
+    This is useful for verifying the mock.
+    """
+    use_real_vuforia = request.param
+    if use_real_vuforia:
+        yield
+    else:
+        with MockVWS():
+            yield
