b159ad5f9f38a49b44ba9c00bac0fd997ec2512b
Adam Dangoor <adamdangoor@gmail.com>
2016-12-30 19:48:41 +0000

    Start of adding add and delete target endpoints to the mock

--- tests/mock_vws/conftest.py
+++ tests/mock_vws/conftest.py
@@ -8,7 +8,7 @@ from typing import Generator
 import pytest
 from _pytest.fixtures import SubRequest
 from requests import codes
-from requests_mock import GET
+from requests_mock import DELETE, GET, POST
 
 from common.constants import ResultCodes
 from mock_vws import MockVWS
@@ -29,6 +29,33 @@ def verify_mock_vuforia(request: SubRequest) -> Generator:
     else:
         with MockVWS():
             yield
+
+
+@pytest.fixture()
+def add_target() -> Endpoint:
+    """
+    Return details of the endpoint for adding a target.
+    """
+    return Endpoint(
+        example_path='/targets',
+        method=POST,
+        successful_headers_status_code=codes.CREATED,
+        successful_headers_result_code=ResultCodes.TARGET_CREATED,
+    )
+
+
+@pytest.fixture()
+def delete_target() -> Endpoint:
+    """
+    Return details of the endpoint for deleting a target.
+    """
+    example_path = '/targets/{target_id}'.format(target_id=uuid.uuid4().hex)
+    return Endpoint(
+        example_path=example_path,
+        method=DELETE,
+        successful_headers_status_code=codes.NOT_FOUND,
+        successful_headers_result_code=ResultCodes.UNKNOWN_TARGET,
+    )
 
 
 @pytest.fixture()
@@ -95,6 +122,7 @@ def endpoint_which_takes_target_id(request: SubRequest) -> Endpoint:
 
 
 @pytest.fixture(params=[
+    'add_target',
     'database_summary',
     'get_duplicates',
     'get_target',
