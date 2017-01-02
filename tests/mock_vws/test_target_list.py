786ad0d2aa0ebd497b5f32122c294f8f0f7e878d
Adam Dangoor <adamdangoor@gmail.com>
2017-01-02 23:24:04 +0000

    Fix sort

--- tests/mock_vws/test_target_list.py
+++ tests/mock_vws/test_target_list.py
@@ -2,15 +2,16 @@
 Tests for the mock of the target list endpoint.
 """
 
+from urllib.parse import urljoin
+
 import pytest
 import requests
 from requests import codes
 from requests_mock import GET
-from urllib.parse import urljoin
 
 from common.constants import ResultCodes
-from tests.utils import VuforiaServerCredentials
 from tests.mock_vws.utils import assert_vws_response
+from tests.utils import VuforiaServerCredentials
 from vws._request_utils import authorization_header, rfc_1123_date
 
 
