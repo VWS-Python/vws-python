7b4fac90cf4af70bb6c634735bde2a1161b3d0ad
Adam Dangoor <adamdangoor@gmail.com>
2017-01-03 02:31:04 +0000

    Revert "Merge remote-tracking branch 'origin/master' into add-target-validation"
    This reverts commit a6bf2ae41900430b9b25d89d89a35fc8831ab698, reversing
    changes made to 6f9d86a42fdf8c226d02b6efb402cda1c9cd8102.

--- tests/mock_vws/test_add_target.py
+++ tests/mock_vws/test_add_target.py
@@ -131,9 +131,6 @@ class TestAddTarget:
     def test_invalid_json(self,
                           vuforia_server_credentials: VuforiaServerCredentials,
                           ) -> None:
-        """
-        A `Fail` result is returned when the data given is not valid JSON.
-        """
         content_type = 'application/json'
         date = rfc_1123_date()
         request_path = '/targets'
@@ -231,7 +228,15 @@ class TestAddTarget:
         date = rfc_1123_date()
         request_path = '/targets'
 
-        content = b'a'
+        image_data = png_file.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+
+        data = {
+            'name': 'example_name_{random}'.format(random=uuid.uuid4().hex),
+            'width': width,
+            'image': image_data_encoded,
+        }
+        content = bytes(json.dumps(data), encoding='utf-8')
 
         authorization_string = authorization_header(
             access_key=vuforia_server_credentials.access_key,
@@ -261,3 +266,186 @@ class TestAddTarget:
             status_code=codes.BAD_REQUEST,
             result_code=ResultCodes.FAIL,
         )
+
+    @pytest.mark.parametrize('data_to_remove', ['name', 'width', 'image'])
+    def test_missing_data(self,
+                          vuforia_server_credentials:
+                          VuforiaServerCredentials,
+                          png_file: io.BytesIO,
+                          data_to_remove: str,
+                          ) -> None:
+        content_type = 'application/json'
+        date = rfc_1123_date()
+        request_path = '/targets'
+
+        image_data = png_file.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+
+        data = {
+            'name': 'example_name_{random}'.format(random=uuid.uuid4().hex),
+            'width': 1,
+            'image': image_data_encoded,
+        }
+        data.pop(data_to_remove)
+        content = bytes(json.dumps(data), encoding='utf-8')
+
+        authorization_string = authorization_header(
+            access_key=vuforia_server_credentials.access_key,
+            secret_key=vuforia_server_credentials.secret_key,
+            method=POST,
+            content=content,
+            content_type=content_type,
+            date=date,
+            request_path=request_path,
+        )
+
+        headers = {
+            "Authorization": authorization_string,
+            "Date": date,
+            'Content-Type': content_type,
+        }
+
+        response = requests.request(
+            method=POST,
+            url=urljoin('https://vws.vuforia.com/', request_path),
+            headers=headers,
+            data=content,
+        )
+
+        assert_vws_failure(
+            response=response,
+            status_code=codes.BAD_REQUEST,
+            result_code=ResultCodes.FAIL,
+        )
+
+    @pytest.mark.parametrize(
+        'name',
+        [1, '', 'a' * 65],
+        ids=['Wrong Type', 'Empty', 'Too Long'],
+    )
+    def test_name_invalid(self,
+                          name: str,
+                          png_file: io.BytesIO,
+                          vuforia_server_credentials: VuforiaServerCredentials
+                          ) -> None:
+        date = rfc_1123_date()
+        request_path = '/targets'
+        content_type = 'application/json'
+
+        image_data = png_file.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+
+        data = {
+            'name': name,
+            'width': 1,
+            'image': image_data_encoded,
+        }
+        content = bytes(json.dumps(data), encoding='utf-8')
+
+        authorization_string = authorization_header(
+            access_key=vuforia_server_credentials.access_key,
+            secret_key=vuforia_server_credentials.secret_key,
+            method=POST,
+            content=content,
+            content_type=content_type,
+            date=date,
+            request_path=request_path,
+        )
+
+        headers = {
+            "Authorization": authorization_string,
+            "Date": date,
+            'Content-Type': content_type,
+        }
+
+        response = requests.request(
+            method=POST,
+            url=urljoin('https://vws.vuforia.com/', request_path),
+            headers=headers,
+            data=content,
+        )
+        assert_vws_failure(
+            response=response,
+            status_code=codes.BAD_REQUEST,
+            result_code=ResultCodes.FAIL,
+        )
+
+
+class TestInvalidImage:
+    # See https://library.vuforia.com/articles/Training/Image-Target-Guide
+
+    # Not JPEG/PNG
+    def test_invalid_type(self) -> None:
+        pass
+
+    # > 2mb
+    def test_too_large(self) -> None:
+        pass
+
+    # Not RGB/greyscale
+    def test_wrong_colour_space(self) -> None:
+        pass
+
+
+class TestNotMandatoryFields:
+
+    def test_invalid_extra_data(self,
+                                vuforia_server_credentials:
+                                VuforiaServerCredentials,
+                                image_file: io.BytesIO,
+                                ) -> None:
+        """XXX"""
+        date = rfc_1123_date()
+        request_path = '/targets'
+        content_type = 'application/json'
+
+        image_data = image_file.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+
+        data = {
+            'name': 'example_name_{random}'.format(random=uuid.uuid4().hex),
+            'width': 1,
+            'image': image_data_encoded,
+            'extra_thing': 1,
+        }
+        content = bytes(json.dumps(data), encoding='utf-8')
+
+        authorization_string = authorization_header(
+            access_key=vuforia_server_credentials.access_key,
+            secret_key=vuforia_server_credentials.secret_key,
+            method=POST,
+            content=content,
+            content_type=content_type,
+            date=date,
+            request_path=request_path,
+        )
+
+        headers = {
+            "Authorization": authorization_string,
+            "Date": date,
+            'Content-Type': content_type,
+        }
+
+        response = requests.request(
+            method=POST,
+            url=urljoin('https://vws.vuforia.com/', request_path),
+            headers=headers,
+            data=content,
+        )
+
+        assert_vws_failure(
+            response=response,
+            status_code=codes.BAD_REQUEST,
+            result_code=ResultCodes.FAIL,
+        )
+
+    def test_valid_extra_data(self) -> None:
+        active_flag = True
+        application_metadata = 'a' # something base64 encoded
+        pass
+
+    def test_invalid_active_flag(self) -> None:
+        pass
+
+    def test_invalid_application_metadata(self) -> None:
+        pass
