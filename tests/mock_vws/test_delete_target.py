e11e12b8c26148b8dedcb9c20d35fcbe2c2d4181
Adam Dangoor <adamdangoor@gmail.com>
2017-01-07 21:16:51 +0000

    Add stub test

---
+++ tests/mock_vws/test_delete_target.py
@@ -0,0 +1,15 @@
+"""
+Tests for deleting targets.
+"""
+
+import pytest
+
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
+class TestDelete:
+    """
+    Tests for deleting targets.
+    """
+
+    def test_delete(self) -> None:
+        pass
