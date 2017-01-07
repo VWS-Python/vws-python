6654d7b0ca973fd605d7a1a4f32fe35562955f4a
Adam Dangoor <adamdangoor@gmail.com>
2017-01-07 21:46:54 +0000

    Start of delete tests

--- tests/mock_vws/test_delete_target.py
+++ tests/mock_vws/test_delete_target.py
@@ -3,6 +3,13 @@ Tests for deleting targets.
 """
 
 import pytest
+from requests import codes
+from requests_mock import DELETE
+
+from common.constants import ResultCodes
+from tests.mock_vws.utils import assert_vws_response
+from vws._request_utils import target_api_request
+from tests.utils import VuforiaServerCredentials
 
 
 @pytest.mark.usefixtures('verify_mock_vuforia')
@@ -11,8 +18,26 @@ class TestDelete:
     Tests for deleting targets.
     """
 
-    def test_delete(self) -> None:
+    def test_delete(self,
+                    target_id: str,
+                    vuforia_server_credentials: VuforiaServerCredentials,
+                    ) -> None:
         """
         It is possible to delete a target.
         """
-        pass
+        request_path = '/targets/' + target_id
+
+        response = target_api_request(
+            access_key=vuforia_server_credentials.access_key,
+            secret_key=vuforia_server_credentials.secret_key,
+            method=DELETE,
+            content=b'',
+            request_path=request_path,
+        )
+
+        # TODO WAIT FOR STATUS!
+        assert_vws_response(
+            response=response,
+            status_code=codes.OK,
+            result_code=ResultCodes.SUCCESS,
+        )
