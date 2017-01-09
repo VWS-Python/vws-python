f1237fdea8bf646b78a32d56bca35c307b500644
Adam Dangoor <adamdangoor@gmail.com>
2017-01-09 02:10:21 +0000

    Basic passing test for getting target

--- tests/mock_vws/test_get_target.py
+++ tests/mock_vws/test_get_target.py
@@ -5,6 +5,12 @@ https://library.vuforia.com/articles/Solution/How-To-Retrieve-a-Target-Record-Us
 """
 
 import pytest
+from requests import codes
+from requests_mock import GET
+
+from common.constants import ResultCodes
+from tests.mock_vws.utils import assert_vws_response, VuforiaServerCredentials
+from vws._request_utils import target_api_request
 
 
 @pytest.mark.usefixtures('verify_mock_vuforia')
@@ -13,8 +19,25 @@ class TestGetRecord:
     Tests for getting a target record.
     """
 
-    def test_get_target(self, target_id: str) -> None:
+    def test_get_target(self,
+                        target_id: str,
+                        vuforia_server_credentials: VuforiaServerCredentials,
+                        ) -> None:
         """
         Details of a target are returned.
         """
-        pass
+        request_path = '/targets/' + target_id
+
+        response = target_api_request(
+            access_key=vuforia_server_credentials.access_key,
+            secret_key=vuforia_server_credentials.secret_key,
+            method=GET,
+            content=b'',
+            request_path=request_path,
+        )
+
+        assert_vws_response(
+            response=response,
+            status_code=codes.OK,
+            result_code=ResultCodes.SUCCESS,
+        )
