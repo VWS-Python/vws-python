3d5946a0c2e7808d602942db469207ffdc787066
Adam Dangoor <adamdangoor@gmail.com>
2017-01-14 20:25:19 +0000

    Class layout for tests

--- tests/mock_vws/test_update_target.py
+++ tests/mock_vws/test_update_target.py
@@ -6,6 +6,38 @@ import pytest
 
 
 @pytest.mark.usefixtures('verify_mock_vuforia')
-class TestDataUpdated:
+class TestName:
     """
+    """
+
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
+class TestWidth:
+    """
+    """
+
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
+class TestImage:
+    """
+    """
+
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
+class TestActiveFlag:
+    """
+    """
+
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
+class TestApplicationMetadata:
+    """
+    Tests for the application metadata parameter.
+    """
+
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
+class TestUnexpectedData:
+    """
+    Tests for passing data which is not mandatory or allowed to the endpoint.
     """
