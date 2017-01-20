bb5775366d72816177295adf784e9ebefb538926
Adam Dangoor <adamdangoor@gmail.com>
2017-01-20 16:28:20 +0000

    Test active flag

--- tests/mock_vws/test_target_summary.py
+++ tests/mock_vws/test_target_summary.py
@@ -2,15 +2,15 @@
 Tests for the mock of the target summary endpoint.
 """
 
-from urllib.parse import urljoin
+import base64
+import io
 
 import pytest
-import requests
 from requests import codes
 from requests_mock import GET
 
 from common.constants import ResultCodes
-from tests.mock_vws.utils import assert_vws_response
+from tests.mock_vws.utils import add_target_to_vws, assert_vws_response
 from tests.utils import VuforiaServerCredentials
 from vws._request_utils import target_api_request
 
@@ -24,19 +24,31 @@ class TestTargetSummary:
     def test_target_summary(
         self,
         vuforia_server_credentials: VuforiaServerCredentials,
-        target_id: str,
+        png_rgb: io.BytesIO,
     ) -> None:
         """
         A target summary is returned.
         """
-        request_path = '/summary/' + target_id
+        name = 'example target name 1234'
+
+        image_data = png_rgb.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+
+        target_response = add_target_to_vws(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data={
+                'name': name,
+                'width': 1,
+                'image': image_data_encoded,
+            }
+        )
 
         response = target_api_request(
             access_key=vuforia_server_credentials.access_key,
             secret_key=vuforia_server_credentials.secret_key,
             method=GET,
             content=b'',
-            request_path=request_path,
+            request_path='/summary/' + target_response.json()['target_id'],
         )
 
         assert_vws_response(
@@ -60,15 +72,39 @@ class TestTargetSummary:
         }
 
         assert response.json().keys() == expected_keys
-
         assert response.json()['status'] == 'processing'
+        assert response.json()['target_name'] == name
 
     @pytest.mark.parametrize('active_flag', [True, False])
     def test_active_flag(
         self,
         vuforia_server_credentials: VuforiaServerCredentials,
+        png_rgb: io.BytesIO,
+        active_flag: bool,
         target_id: str,
     ) -> None:
         """
-        XXX
+        The active flag of the target is returned.
         """
+        image_data = png_rgb.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+
+        target_response = add_target_to_vws(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data={
+                'name': 'example',
+                'width': 1,
+                'image': image_data_encoded,
+                'active_flag': active_flag,
+            }
+        )
+
+        response = target_api_request(
+            access_key=vuforia_server_credentials.access_key,
+            secret_key=vuforia_server_credentials.secret_key,
+            method=GET,
+            content=b'',
+            request_path='/summary/' + target_response.json()['target_id'],
+        )
+
+        assert response.json()['active_flag'] == active_flag
