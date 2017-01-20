0ce8b939a8318e1369f7868887c5d8eba730ada0
Adam Dangoor <adamdangoor@gmail.com>
2017-01-20 23:20:31 +0000

    Test target name

--- tests/mock_vws/test_target_summary.py
+++ tests/mock_vws/test_target_summary.py
@@ -2,17 +2,17 @@
 Tests for the mock of the target summary endpoint.
 """
 
-from urllib.parse import urljoin
+import base64
+import io
 
 import pytest
-import requests
 from requests import codes
 from requests_mock import GET
 
 from common.constants import ResultCodes
-from tests.mock_vws.utils import assert_vws_response
+from tests.mock_vws.utils import add_target_to_vws, assert_vws_response
 from tests.utils import VuforiaServerCredentials
-from vws._request_utils import authorization_header, rfc_1123_date
+from vws._request_utils import target_api_request
 
 
 @pytest.mark.usefixtures('verify_mock_vuforia')
@@ -24,36 +24,31 @@ class TestTargetSummary:
     def test_target_summary(
         self,
         vuforia_server_credentials: VuforiaServerCredentials,
-        target_id: str,
+        png_rgb: io.BytesIO,
     ) -> None:
         """
         A target summary is returned.
         """
-        content_type = 'application/json'
-        date = rfc_1123_date()
-        request_path = '/summary/' + target_id
+        name = 'example target name 1234'
 
-        authorization_string = authorization_header(
+        image_data = png_rgb.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+
+        target_response = add_target_to_vws(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data={
+                'name': name,
+                'width': 1,
+                'image': image_data_encoded,
+            }
+        )
+
+        response = target_api_request(
             access_key=vuforia_server_credentials.access_key,
             secret_key=vuforia_server_credentials.secret_key,
             method=GET,
             content=b'',
-            content_type=content_type,
-            date=date,
-            request_path=request_path,
-        )
-
-        headers = {
-            "Authorization": authorization_string,
-            "Date": date,
-            'Content-Type': content_type,
-        }
-
-        response = requests.request(
-            method=GET,
-            url=urljoin('https://vws.vuforia.com/', request_path),
-            headers=headers,
-            data='',
+            request_path='/summary/' + target_response.json()['target_id'],
         )
 
         assert_vws_response(
@@ -77,5 +72,5 @@ class TestTargetSummary:
         }
 
         assert response.json().keys() == expected_keys
-
         assert response.json()['status'] == 'processing'
+        assert response.json()['target_name'] == name
