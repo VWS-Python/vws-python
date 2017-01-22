c845f602aa4035d4d9eceaae17b4fddffecaf5ab
Adam Dangoor <adamdangoor@gmail.com>
2017-01-22 21:09:52 +0000

    FIx pylint

--- tests/mock_vws/test_get_target.py
+++ tests/mock_vws/test_get_target.py
@@ -219,7 +219,7 @@ class TestGetRecord:
 
         target_id = response.json()['target_id']
 
-        for i in range(20):
+        for _ in range(20):
             response = get_target(
                 target_id=target_id,
                 vuforia_server_credentials=vuforia_server_credentials
