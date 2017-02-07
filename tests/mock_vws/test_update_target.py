3371bf327ec42b8b963149d13fb12a89556152d2
Adam Dangoor <adamdangoor@gmail.com>
2017-02-07 15:11:29 +0000

    Outline for metadata in update

--- tests/mock_vws/test_update_target.py
+++ tests/mock_vws/test_update_target.py
@@ -3,6 +3,7 @@ Tests for the mock of the update target endpoint.
 """
 
 import base64
+import binascii
 import io
 import json
 from typing import Any, Dict, Union
@@ -341,5 +342,121 @@ class TestActiveFlag:
         assert_vws_response(
             response=response,
             status_code=codes.BAD_REQUEST,
+            result_code=ResultCodes.FAIL,
+        )
+
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
+class TestApplicationMetadata:
+    """
+    Tests for the application metadata parameter.
+    """
+
+    def test_base64_encoded(
+        self,
+        vuforia_server_credentials: VuforiaServerCredentials,
+        target_id: str,
+    ) -> None:
+        """
+        A base64 encoded string is valid application metadata.
+        """
+        metadata = b'Some data'
+        metadata_encoded = base64.b64encode(metadata).decode('ascii')
+
+        wait_for_target_processed(
+            vuforia_server_credentials=vuforia_server_credentials,
+            target_id=target_id,
+        )
+
+        response = update_target(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data={'application_metadata': metadata_encoded},
+            target_id=target_id,
+        )
+
+        assert_vws_response(
+            response=response,
+            status_code=codes.OK,
+            result_code=ResultCodes.SUCCESS,
+        )
+
+    def test_null(
+        self,
+        vuforia_server_credentials: VuforiaServerCredentials,
+        target_id: str,
+    ) -> None:
+        """
+        NULL is valid application metadata.
+        """
+        wait_for_target_processed(
+            vuforia_server_credentials=vuforia_server_credentials,
+            target_id=target_id,
+        )
+
+        response = update_target(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data={'application_metadata': None},
+            target_id=target_id,
+        )
+
+        assert_vws_response(
+            response=response,
+            status_code=codes.OK,
+            result_code=ResultCodes.SUCCESS,
+        )
+
+    def test_invalid_type(
+        self,
+        vuforia_server_credentials: VuforiaServerCredentials,
+        target_id: str,
+    ) -> None:
+        """
+        Values which are not a string or NULL are not valid application
+        metadata.
+        """
+        wait_for_target_processed(
+            vuforia_server_credentials=vuforia_server_credentials,
+            target_id=target_id,
+        )
+
+        response = update_target(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data={'application_metadata': 1},
+            target_id=target_id,
+        )
+
+        assert_vws_failure(
+            response=response,
+            status_code=codes.BAD_REQUEST,
+            result_code=ResultCodes.FAIL,
+        )
+
+    def test_not_base64_encoded(
+        self,
+        vuforia_server_credentials: VuforiaServerCredentials,
+        target_id: str,
+    ) -> None:
+        """
+        A string which is not base64 encoded is not valid application metadata.
+        """
+        not_base64_encoded = b'a'
+
+        with pytest.raises(binascii.Error):
+            base64.b64decode(not_base64_encoded)
+
+        wait_for_target_processed(
+            vuforia_server_credentials=vuforia_server_credentials,
+            target_id=target_id,
+        )
+
+        response = update_target(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data={'application_metadata': not_base64_encoded},
+            target_id=target_id,
+        )
+
+        assert_vws_failure(
+            response=response,
+            status_code=codes.UNPROCESSABLE_ENTITY,
             result_code=ResultCodes.FAIL,
         )
