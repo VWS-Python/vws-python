0b1e4fc7c74aea30f86d21721cdac070ade3c66d
Adam Dangoor <adamdangoor@gmail.com>
2017-02-11 16:29:17 +0000

    Progress towards test for inactive

--- tests/mock_vws/test_inactive_project.py
+++ tests/mock_vws/test_inactive_project.py
@@ -4,10 +4,12 @@ Tests for inactive projects.
 
 import pytest
 
+from tests.mock_vws.utils import Endpoint
 from tests.utils import VuforiaServerCredentials
+from vws.request_utils import target_api_request
 
 
-@pytest.mark.usefixtures('verify_mock_vuforia')
+@pytest.mark.usefixtures('verify_mock_vuforia_inactive')
 class TestInactiveProject:
     """
     Tests for inactive projects.
@@ -15,8 +17,18 @@ class TestInactiveProject:
 
     def test_inactive_project(
         self,
-        verify_mock_vuforia_inactive: VuforiaServerCredentials,
+        endpoint: Endpoint,
+        vuforia_inactive_server_credentials: VuforiaServerCredentials,
     ) -> None:
         """
         X
         """
+        response = target_api_request(
+            access_key=vuforia_inactive_server_credentials.access_key,
+            secret_key=vuforia_inactive_server_credentials.secret_key,
+            method=endpoint.method,
+            content=endpoint.content,
+            request_path=endpoint.example_path,
+        )
+
+        import pdb; pdb.set_trace()
