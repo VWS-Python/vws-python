f0daa1674ef6fd59173fe7d1904f8598f7418f8b
Adam Dangoor <adamdangoor@gmail.com>
2017-02-11 16:17:42 +0000

    Add stub for inactive project test

--- tests/mock_vws/test_inactive_project.py
+++ tests/mock_vws/test_inactive_project.py
@@ -1,3 +1,22 @@
 """
 Tests for inactive projects.
 """
+
+import pytest
+
+from tests.utils import VuforiaServerCredentials
+
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
+class TestInactiveProject:
+    """
+    Tests for inactive projects.
+    """
+
+    def test_inactive_project(
+        self,
+        verify_mock_vuforia_inactive: VuforiaServerCredentials,
+    ) -> None:
+        """
+        X
+        """
