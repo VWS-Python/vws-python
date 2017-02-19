8c243082ffb3a2d38de3842a9cf65e3262982d97
Adam Dangoor <adamdangoor@gmail.com>
2017-02-19 23:47:23 +0000

    Start of stub test

--- tests/mock_vws/test_get_duplicates.py
+++ tests/mock_vws/test_get_duplicates.py
@@ -1,3 +1,19 @@
 """
 Tests for the mock of the get duplicates endpoint.
 """
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
+class TestDuplicates:
+    """
+    Tests for the mock of the target duplicates endpoint.
+    """
+
+    def test_no_duplicates(
+        self,
+        vuforia_server_credentials: VuforiaServerCredentials,
+        target_id: str,
+    ):
+        """
+        XXX
+        """
+        pass
