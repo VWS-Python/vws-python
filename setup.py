10b45cbc0867161ea7cb3878df9c4cdbdc2c393a
Adam Dangoor <adamdangoor@gmail.com>
2017-12-22 05:27:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ from setuptools import find_packages, setup
 
 # We use requirements.txt instead of just declaring the requirements here
 # because this helps with Docker package caching.
-with open("requirements.txt") as requirements:
+with open('requirements.txt') as requirements:
     install_requires = requirements.readlines()
 
 # We use dev-requirements.txt instead of just declaring the requirements here
 # because Read The Docs needs a requirements file.
-with open("dev-requirements.txt") as dev_requirements:
+with open('dev-requirements.txt') as dev_requirements:
     dev_requires = dev_requirements.readlines()
 
 with open('README.md') as f:
     long_description = f.read()
 
 setup(
-    name="VWS Python",
-    version="0.1",
-    author="Adam Dangoor",
+    name='VWS Python',
+    version='0.1',
+    author='Adam Dangoor',
     author_email='adamdangoor@gmail.com',
-    description="Interact with the Vuforia Web Services (VWS) API.",
+    description='Interact with the Vuforia Web Services (VWS) API.',
     long_description=long_description,
     license='MIT',
     packages=find_packages(where='src', exclude='common'),
@@ -30,7 +30,7 @@ setup(
     package_dir={'': 'src'},
     install_requires=install_requires,
     extras_require={
-        "dev": dev_requires,
+        'dev': dev_requires,
     },
     classifiers=[
         'Operating System :: POSIX',
