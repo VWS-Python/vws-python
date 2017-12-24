50a457ff7e4766ed9770bf47acc68370e84e3ae8
Adam Dangoor <adamdangoor@gmail.com>
2017-12-24 04:08:44 +0000

    Start of new test

--- tests/mock_vws/test_get_duplicates.py
+++ tests/mock_vws/test_get_duplicates.py
@@ -113,3 +113,62 @@ class TestDuplicates:
         assert response.json().keys() == expected_keys
 
         assert response.json()['similar_targets'] == [similar_target_id]
+
+    def test_processing(
+        self,
+        vuforia_server_credentials: VuforiaServerCredentials,
+        high_quality_image: io.BytesIO,
+    ) -> None:
+        """
+        Targets are not duplicates while they are being processed.
+        """
+        image_data = high_quality_image.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+
+        original_data = {
+            'name': str(uuid.uuid4()),
+            'width': 1,
+            'image': image_data_encoded,
+        }
+
+        similar_data = {
+            'name': str(uuid.uuid4()),
+            'width': 1,
+            'image': image_data_encoded,
+        }
+
+        original_add_resp = add_target_to_vws(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data=original_data,
+        )
+
+        add_target_to_vws(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data=similar_data,
+        )
+
+        original_target_id = original_add_resp.json()['target_id']
+
+        response = target_api_request(
+            access_key=vuforia_server_credentials.access_key,
+            secret_key=vuforia_server_credentials.secret_key,
+            method=GET,
+            content=b'',
+            request_path='/duplicates/' + original_target_id,
+        )
+
+        assert_vws_response(
+            response=response,
+            status_code=codes.OK,
+            result_code=ResultCodes.SUCCESS,
+        )
+
+        expected_keys = {
+            'result_code',
+            'transaction_id',
+            'similar_targets',
+        }
+
+        assert response.json().keys() == expected_keys
+
+        assert response.json()['similar_targets'] == []
