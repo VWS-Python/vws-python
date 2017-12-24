6aca8d9d34d7621409d024aacf20d5bce74a069e
Adam Dangoor <adamdangoor@gmail.com>
2017-12-24 01:52:21 +0000

    Combine tests

--- tests/mock_vws/test_get_duplicates.py
+++ tests/mock_vws/test_get_duplicates.py
@@ -2,6 +2,8 @@
 Tests for the mock of the get duplicates endpoint.
 """
 
+import base64
+import io
 import uuid
 
 import pytest
@@ -9,7 +11,7 @@ from requests import codes
 from requests_mock import GET
 
 from common.constants import ResultCodes
-from tests.mock_vws.utils import assert_vws_response
+from tests.mock_vws.utils import add_target_to_vws, assert_vws_response
 from tests.utils import VuforiaServerCredentials
 from vws._request_utils import target_api_request
 
@@ -20,39 +22,6 @@ class TestDuplicates:
     Tests for the mock of the target duplicates endpoint.
     """
 
-    def test_no_duplicates(
-        self,
-        vuforia_server_credentials: VuforiaServerCredentials,
-        target_id: str,
-    ) -> None:
-        """
-        If there are no similar images to the given target, an empty list is
-        returned.
-        """
-        response = target_api_request(
-            access_key=vuforia_server_credentials.access_key,
-            secret_key=vuforia_server_credentials.secret_key,
-            method=GET,
-            content=b'',
-            request_path='/duplicates/' + target_id,
-        )
-
-        assert_vws_response(
-            response=response,
-            status_code=codes.OK,
-            result_code=ResultCodes.SUCCESS,
-        )
-
-        expected_keys = {
-            'result_code',
-            'transaction_id',
-            'similar_targets',
-        }
-
-        assert response.json().keys() == expected_keys
-
-        assert response.json()['similar_targets'] == []
-
     def test_duplicates(
         self,
         vuforia_server_credentials: VuforiaServerCredentials,
@@ -60,14 +29,10 @@ class TestDuplicates:
         png_greyscale: io.BytesIO,
     ) -> None:
         """
-        XXX
+        Target IDs of similar targets are returned.
+
+        In the mock, "similar" means that the images are exactly the same.
         """
-        first_target = 'X'
-
-        similar_target = 'X'
-
-        different_target = 'X'
-
         image_data = png_rgb.read()
         image_data_encoded = base64.b64encode(image_data).decode('ascii')
 
@@ -92,12 +57,12 @@ class TestDuplicates:
             'image': different_data_encoded,
         }
 
-        add_target_to_vws(
+        original_add_resp = add_target_to_vws(
             vuforia_server_credentials=vuforia_server_credentials,
             data=original_data,
         )
 
-        add_target_to_vws(
+        similar_add_resp = add_target_to_vws(
             vuforia_server_credentials=vuforia_server_credentials,
             data=similar_data,
         )
@@ -107,12 +72,15 @@ class TestDuplicates:
             data=different_data,
         )
 
+        original_target_id = original_add_resp.json()['target_id']
+        similar_target_id = similar_add_resp.json()['target_id']
+
         response = target_api_request(
             access_key=vuforia_server_credentials.access_key,
             secret_key=vuforia_server_credentials.secret_key,
             method=GET,
             content=b'',
-            request_path='/duplicates/' + target_id,
+            request_path='/duplicates/' + original_target_id,
         )
 
         assert_vws_response(
