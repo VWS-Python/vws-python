f723352816f14b71ca1ef06790a3c6c2a45ea630
Adam Dangoor <adamdangoor@gmail.com>
2017-12-25 11:00:33 +0000

    Stub for query tests

---
+++ tests/mock_vws/test_query.py
@@ -0,0 +1,17 @@
+"""
+Tests for the mock of the query endpoint.
+
+https://library.vuforia.com/articles/Solution/How-To-Perform-an-Image-Recognition-Query.
+"""
+
+class TestQuery:
+    """
+    XXX
+    """
+
+    def test_query(self):
+        """
+        XXX
+        """
+        pass
+
