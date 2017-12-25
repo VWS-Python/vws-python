7aff9e2becbd31a98a0d4ba6ecb5af878fab5e97
Adam Dangoor <adamdangoor@gmail.com>
2017-12-25 12:11:42 +0000

    Add image in test

--- tests/mock_vws/test_query.py
+++ tests/mock_vws/test_query.py
@@ -9,9 +9,42 @@ class TestQuery:
     XXX
     """
 
-    def test_query(self):
+    """
+    * Normal query
+    * Maximum size PNG
+    * Maximum size JPG
+    * max_num_results
+    * include_target_data
+    * Active flag
+    * Success: False
+    """
+
+    def test_query(
+        self,
+        vuforia_server_credentials: VuforiaServerCredentials,
+        png_rgb_success: io.BytesIO,
+    ):
         """
         XXX
         """
-        pass
+        image_data = png_rgb_success.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+
+        data = {
+            'name': 'my_example_name',
+            'width': 1234,
+            'image': image_data_encoded,
+        }
+
+        response = add_target_to_vws(
+            vuforia_server_credentials=vuforia_server_credentials,
+            data=data,
+        )
+
+        target_id = response.json()['target_id']
+
+        wait_for_target_processed(
+            vuforia_server_credentials=vuforia_server_credentials,
+            target_id=target_id,
+        )
 
