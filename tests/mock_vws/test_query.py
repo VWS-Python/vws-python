b0d05b4f8c129d83eabdbbd0855869285c978e27
Adam Dangoor <adamdangoor@gmail.com>
2017-12-25 20:56:44 +0000

    Progress towards stub

--- tests/mock_vws/test_query.py
+++ tests/mock_vws/test_query.py
@@ -4,53 +4,20 @@ Tests for the mock of the query endpoint.
 https://library.vuforia.com/articles/Solution/How-To-Perform-an-Image-Recognition-Query.
 """
 
-def query_vws(
-    vuforia_server_credentials: VuforiaServerCredentials,
-    data: Dict[str, Any],
-    content_type: str = 'application/json',
-) -> Response:
-    """
-    Return a response from a request to the endpoint to add a target.
+import io
+from typing import Any, Dict
+from urllib.parse import urljoin
 
-    Args:
-        vuforia_server_credentials: The credentials to use to connect to
-            Vuforia.
-        data: The data to send, in JSON format, to the endpoint.
-        content_type: The `Content-Type` header to use.
+import pytest
+import requests
+from requests import Response, codes
+from requests_mock import POST
 
-    Returns:
-        The response returned by the API.
-    """
-    date = rfc_1123_date()
-    request_path = '/targets'
+from tests.utils import VuforiaServerCredentials
+from vws._request_utils import authorization_header, rfc_1123_date
+import json
 
-    content = bytes(json.dumps(data), encoding='utf-8')
-
-    authorization_string = authorization_header(
-        access_key=vuforia_server_credentials.access_key,
-        secret_key=vuforia_server_credentials.secret_key,
-        method=POST,
-        content=content,
-        content_type=content_type,
-        date=date,
-        request_path=request_path,
-    )
-
-    headers = {
-        'Authorization': authorization_string,
-        'Date': date,
-        'Content-Type': content_type,
-    }
-
-    response = requests.request(
-        method=POST,
-        url=urljoin('https://vws.vuforia.com/', request_path),
-        headers=headers,
-        data=content,
-    )
-
-    return response
-
+@pytest.mark.usefixtures('verify_mock_vuforia')
 class TestQuery:
     """
     XXX
@@ -75,27 +42,36 @@ class TestQuery:
         """
         XXX
         """
-        image_data = png_rgb_success.read()
-        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+        date = rfc_1123_date()
+        request_path = '/v1/query'
+        content_type = 'multipart/form-data'
 
-        data = {
-            'name': 'my_example_name',
-            'width': 1234,
-            'image': image_data_encoded,
+        data = {}
+
+        content = bytes(json.dumps(data), encoding='utf-8')
+
+        authorization_string = authorization_header(
+            access_key=vuforia_server_credentials.access_key,
+            secret_key=vuforia_server_credentials.secret_key,
+            method=POST,
+            content=content,
+            content_type=content_type,
+            date=date,
+            request_path=request_path,
+        )
+
+        headers = {
+            'Authorization': authorization_string,
+            'Date': date,
+            'Content-Type': content_type,
         }
 
-        response = add_target_to_vws(
-            vuforia_server_credentials=vuforia_server_credentials,
-            data=data,
+        response = requests.request(
+            method=POST,
+            url=urljoin('https://cloudreco.vuforia.com', request_path),
+            headers=headers,
+            data=content,
         )
-
-        target_id = response.json()['target_id']
-
-        wait_for_target_processed(
-            vuforia_server_credentials=vuforia_server_credentials,
-            target_id=target_id,
-        )
-
-        response = query()
 
         assert 'results' not in response.json()
+        import pdb; pdb.set_trace()
