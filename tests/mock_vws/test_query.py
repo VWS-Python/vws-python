d88cf7e239a012952733f3bbe029cbc0dbc65626
Adam Dangoor <adamdangoor@gmail.com>
2017-12-25 19:29:45 +0000

    Progress towards query

--- tests/mock_vws/test_query.py
+++ tests/mock_vws/test_query.py
@@ -4,6 +4,53 @@ Tests for the mock of the query endpoint.
 https://library.vuforia.com/articles/Solution/How-To-Perform-an-Image-Recognition-Query.
 """
 
+def query_vws(
+    vuforia_server_credentials: VuforiaServerCredentials,
+    data: Dict[str, Any],
+    content_type: str = 'application/json',
+) -> Response:
+    """
+    Return a response from a request to the endpoint to add a target.
+
+    Args:
+        vuforia_server_credentials: The credentials to use to connect to
+            Vuforia.
+        data: The data to send, in JSON format, to the endpoint.
+        content_type: The `Content-Type` header to use.
+
+    Returns:
+        The response returned by the API.
+    """
+    date = rfc_1123_date()
+    request_path = '/targets'
+
+    content = bytes(json.dumps(data), encoding='utf-8')
+
+    authorization_string = authorization_header(
+        access_key=vuforia_server_credentials.access_key,
+        secret_key=vuforia_server_credentials.secret_key,
+        method=POST,
+        content=content,
+        content_type=content_type,
+        date=date,
+        request_path=request_path,
+    )
+
+    headers = {
+        'Authorization': authorization_string,
+        'Date': date,
+        'Content-Type': content_type,
+    }
+
+    response = requests.request(
+        method=POST,
+        url=urljoin('https://vws.vuforia.com/', request_path),
+        headers=headers,
+        data=content,
+    )
+
+    return response
+
 class TestQuery:
     """
     XXX
@@ -12,6 +59,7 @@ class TestQuery:
     """
     * Normal query
     * Maximum size PNG
+    * Bad content type
     * Maximum size JPG
     * max_num_results
     * include_target_data
@@ -19,7 +67,7 @@ class TestQuery:
     * Success: False
     """
 
-    def test_query(
+    def test_no_results(
         self,
         vuforia_server_credentials: VuforiaServerCredentials,
         png_rgb_success: io.BytesIO,
