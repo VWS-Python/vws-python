03b4525bc327e22318c3fe95717b07f0548e32ff
Adam Dangoor <adamdangoor@gmail.com>
2017-12-27 00:29:59 +0000

    Start of adding client keys

--- tests/mock_vws/test_usage.py
+++ tests/mock_vws/test_usage.py
@@ -74,6 +74,8 @@ def assert_valid_server_credentials(
         database_name=uuid.uuid4().hex,
         server_access_key=server_access_key,
         server_secret_key=server_secret_key,
+        client_access_key=uuid.uuid4().hex,
+        client_secret_key=uuid.uuid4().hex,
     )
 
     response = get_vws_target(
