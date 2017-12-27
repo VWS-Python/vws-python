c3b4aefcc64bd9c282ce5aa7d0aadeda2dd158bb
Adam Dangoor <adamdangoor@gmail.com>
2017-12-27 00:38:46 +0000

    Add client keys to keys

--- tests/mock_vws/test_usage.py
+++ tests/mock_vws/test_usage.py
@@ -135,6 +135,8 @@ class TestDatabaseName:
                 database_name=uuid.uuid4().hex,
                 server_access_key=mock.server_access_key,
                 server_secret_key=mock.server_secret_key,
+                client_access_key=uuid.uuid4().hex,
+                client_secret_key=uuid.uuid4().hex,
             )
 
             response = database_summary(
@@ -147,6 +149,8 @@ class TestDatabaseName:
                 database_name=uuid.uuid4().hex,
                 server_access_key=mock.server_access_key,
                 server_secret_key=mock.server_secret_key,
+                client_access_key=uuid.uuid4().hex,
+                client_secret_key=uuid.uuid4().hex,
             )
             response = database_summary(
                 vuforia_database_keys=vuforia_database_keys,
@@ -168,6 +172,8 @@ class TestDatabaseName:
                 database_name=database_name,
                 server_access_key=mock.server_access_key,
                 server_secret_key=mock.server_secret_key,
+                client_access_key=uuid.uuid4().hex,
+                client_secret_key=uuid.uuid4().hex,
             )
             response = database_summary(
                 vuforia_database_keys=vuforia_database_keys,
@@ -202,6 +208,8 @@ class TestPersistence:
                 database_name=uuid.uuid4().hex,
                 server_access_key=mock.server_access_key,
                 server_secret_key=mock.server_secret_key,
+                client_access_key=uuid.uuid4().hex,
+                client_secret_key=uuid.uuid4().hex,
             )
 
             response = add_target_to_vws(
@@ -223,6 +231,8 @@ class TestPersistence:
                 database_name=uuid.uuid4().hex,
                 server_access_key=mock.server_access_key,
                 server_secret_key=mock.server_secret_key,
+                client_access_key=uuid.uuid4().hex,
+                client_secret_key=uuid.uuid4().hex,
             )
 
             response = get_vws_target(
