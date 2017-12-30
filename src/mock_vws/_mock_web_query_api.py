1c4851ff5dd624cce4bc237736550690bafa98eb
Adam Dangoor <adamdangoor@gmail.com>
2017-12-30 22:09:09 +0000

    Move route to common module

--- src/mock_vws/_mock_web_query_api.py
+++ src/mock_vws/_mock_web_query_api.py
@@ -4,3 +4,28 @@ A fake implementation of the Vuforia Web Query API.
 See
 https://library.vuforia.com/articles/Solution/How-To-Perform-an-Image-Recognition-Query
 """
+
+
+class MockVuforiaWebQueryAPI:
+    """
+    XXX
+    """
+
+    def __init__(
+        self,
+        client_access_key: str,
+        server_secret_key: str,
+        mock_target_api: MockVuforiaWebServicesAPI,
+    ) -> None:
+        """
+        XXX
+        """
+
+    def query(
+        self,
+        request: _RequestObjectProxy,
+        context: _Context,
+    ) -> str:
+        """
+        XXX
+        """
--- src/mock_vws/_mock_web_services_api.py
+++ src/mock_vws/_mock_web_services_api.py
@@ -23,6 +23,7 @@ from requests_mock.request import _RequestObjectProxy
 from requests_mock.response import _Context
 
 from mock_vws._constants import ResultCodes, TargetStatuses
+from mock_vws._mock_common import Route
 
 from ._constants import States
 from ._validators import (
@@ -119,40 +120,6 @@ def headers(
     date = email.utils.formatdate(None, localtime=False, usegmt=True)
     context.headers['Date'] = date
     return result
-
-
-class Route:
-    """
-    A container for the route details which `requests_mock` needs.
-
-    We register routes with names, and when we have an instance to work with
-    later.
-    """
-
-    def __init__(
-        self,
-        route_name: str,
-        path_pattern: str,
-        http_methods: List[str],
-    ) -> None:
-        """
-        Args:
-            route_name: The name of the method.
-            path_pattern: The end part of a URL pattern. E.g. `/targets` or
-                `/targets/.+`.
-            http_methods: HTTP methods that map to the route function.
-
-        Attributes:
-            route_name: The name of the method.
-            path_pattern: The end part of a URL pattern. E.g. `/targets` or
-                `/targets/.+`.
-            http_methods: HTTP methods that map to the route function.
-            endpoint: The method `requests_mock` should call when the endpoint
-                is requested.
-        """
-        self.route_name = route_name
-        self.path_pattern = path_pattern
-        self.http_methods = http_methods
 
 
 ROUTES = set([])
