180dd023bd7e45d8f04eea9fea7767ab9114376f
Adam Dangoor <adamdangoor@gmail.com>
2018-01-06 13:27:33 +0000

    Prepare request for get target

--- tests/mock_vws/conftest.py
+++ tests/mock_vws/conftest.py
@@ -548,18 +548,47 @@ def get_duplicates(
 
 
 @pytest.fixture()
-def get_target() -> TargetAPIEndpoint:
+def get_target(
+    vuforia_database_keys: VuforiaDatabaseKeys,
+) -> TargetAPIEndpoint:
     """
     Return details of the endpoint for getting details of a target.
     """
-    example_path = '/targets/{target_id}'.format(target_id=uuid.uuid4().hex)
+    date = rfc_1123_date()
+    target_id = uuid.uuid4().hex
+    request_path = f'/targets/{target_id}'
+    method = GET
+
+    content = b''
+
+    authorization_string = authorization_header(
+        access_key=vuforia_database_keys.server_access_key,
+        secret_key=vuforia_database_keys.server_secret_key,
+        method=method,
+        content=content,
+        content_type='',
+        date=date,
+        request_path=request_path,
+    )
+
+    headers = {
+        'Authorization': authorization_string,
+        'Date': date,
+    }
+
+    request = requests.Request(
+        method=method,
+        url=urljoin(base=VWS_HOST, url=request_path),
+        headers=headers,
+        data=content,
+    )
+
+    prepared_request = request.prepare()  # type: ignore
+
     return TargetAPIEndpoint(
-        example_path=example_path,
-        method=GET,
         successful_headers_status_code=codes.NOT_FOUND,
         successful_headers_result_code=ResultCodes.UNKNOWN_TARGET,
-        content_type=None,
-        content=b'',
+        prepared_request=prepared_request,
     )
 
 
@@ -670,7 +699,7 @@ def endpoint_which_takes_data(request: SubRequest) -> TargetAPIEndpoint:
         'database_summary',
         'delete_target',
         'get_duplicates',
-        # 'get_target',
+        'get_target',
         # 'target_list',
         # 'target_summary',
         # 'update_target',
