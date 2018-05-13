826b5b4ffec7b655fc90fcfc629a990726a2718f
Adam Dangoor <adamdangoor@gmail.com>
2018-05-13 14:56:11 +0100

    Passing test on real implementation for out of range max num

--- tests/mock_vws/test_query.py
+++ tests/mock_vws/test_query.py
@@ -9,12 +9,14 @@ from urllib.parse import urljoin
 
 import pytest
 import requests
+from requests import codes
 from requests_mock import POST
 from urllib3.filepost import encode_multipart_formdata
 
 from tests.mock_vws.utils import (
     VuforiaDatabaseKeys,
     assert_query_success,
+    assert_vwq_failure,
     authorization_header,
     rfc_1123_date,
 )
@@ -147,45 +149,68 @@ class TestMaxNumResults:
         assert_query_success(response=response)
         assert response.json()['results'] == []
 
-    # def test_invalid(self):
-    #     """
-    #     XXX
-    #     """
-    #     image_content = high_quality_image.read()
-    #     date = rfc_1123_date()
-    #     request_path = '/v1/query'
-    #     body = {
-    #         'image': ('image.jpeg', image_content, 'image/jpeg'),
-    #         'max_num_results': ('image.jpeg', image_content, 'image/jpeg'),
-    #     }
-    #     content, content_type_header = encode_multipart_formdata(body)
-    #     method = POST
-    #
-    #     access_key = vuforia_database_keys.client_access_key
-    #     secret_key = vuforia_database_keys.client_secret_key
-    #     authorization_string = authorization_header(
-    #         access_key=access_key,
-    #         secret_key=secret_key,
-    #         method=method,
-    #         content=content,
-    #         # Note that this is not the actual Content-Type header value sent.
-    #         content_type='multipart/form-data',
-    #         date=date,
-    #         request_path=request_path,
-    #     )
-    #
-    #     headers = {
-    #         'Authorization': authorization_string,
-    #         'Date': date,
-    #         'Content-Type': content_type_header,
-    #     }
-    #
-    #     response = requests.request(
-    #         method=method,
-    #         url=urljoin(base=VWQ_HOST, url=request_path),
-    #         headers=headers,
-    #         data=content,
-    #     )
-    #
-    #     assert_query_success(response=response)
-    #     assert response.json()['results'] == []
+    @pytest.mark.parametrize('num_results', [0, 51])
+    def test_out_of_range(
+        self,
+        high_quality_image: io.BytesIO,
+        vuforia_database_keys: VuforiaDatabaseKeys,
+        num_results: int,
+    ) -> None:
+        """
+        XXX
+        """
+        image_content = high_quality_image.read()
+        date = rfc_1123_date()
+        request_path = '/v1/query'
+        body = {
+            'image': ('image.jpeg', image_content, 'image/jpeg'),
+            'max_num_results': (None, num_results, 'text/plain'),
+        }
+        content, content_type_header = encode_multipart_formdata(body)
+        method = POST
+
+        access_key = vuforia_database_keys.client_access_key
+        secret_key = vuforia_database_keys.client_secret_key
+        authorization_string = authorization_header(
+            access_key=access_key,
+            secret_key=secret_key,
+            method=method,
+            content=content,
+            # Note that this is not the actual Content-Type header value sent.
+            content_type='multipart/form-data',
+            date=date,
+            request_path=request_path,
+        )
+
+        headers = {
+            'Authorization': authorization_string,
+            'Date': date,
+            'Content-Type': content_type_header,
+        }
+
+        response = requests.request(
+            method=method,
+            url=urljoin(base=VWQ_HOST, url=request_path),
+            headers=headers,
+            data=content,
+        )
+
+        expected_text = (
+            f'Integer out of range ({num_results}) in form data part '
+            "'max_result'. Accepted range is from 1 to 50 (inclusive)."
+        )
+        assert response.text == expected_text
+        assert_vwq_failure(
+            response=response,
+            content_type='application/json',
+            status_code=codes.BAD_REQUEST,
+        )
+
+
+    def test_invalid_type(
+        self,
+    ) -> None:
+        """
+        See https://github.com/adamtheturtle/vws-python/issues/357 for
+        implementing this test.
+        """
