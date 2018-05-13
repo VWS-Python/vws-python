76ba4dbe4b7aff46e8838aeea186821a0b9150fa
Adam Dangoor <adamdangoor@gmail.com>
2018-05-13 19:37:43 +0100

    Test passing on real for wrong type

--- tests/mock_vws/test_query.py
+++ tests/mock_vws/test_query.py
@@ -5,6 +5,7 @@ https://library.vuforia.com/articles/Solution/How-To-Perform-an-Image-Recognitio
 """
 
 import io
+from typing import Union
 from urllib.parse import urljoin
 
 import pytest
@@ -90,12 +91,12 @@ class TestMaxNumResults:
         implementing this test.
         """
 
-    @pytest.mark.parametrize('num_results', [1, 50])
+    @pytest.mark.parametrize('num_results', [1, b'1', 50])
     def test_valid(
         self,
         high_quality_image: io.BytesIO,
         vuforia_database_keys: VuforiaDatabaseKeys,
-        num_results: int,
+        num_results: Union[int, bytes],
     ) -> None:
         """
         See https://github.com/adamtheturtle/vws-python/issues/357 for
@@ -153,11 +154,11 @@ class TestMaxNumResults:
         self,
         high_quality_image: io.BytesIO,
         vuforia_database_keys: VuforiaDatabaseKeys,
-        num_results: int,
+        num_results: Union[int, bytes],
     ) -> None:
         """
-        An error is returned if ``max_num_results`` is given as an integer
-        out of the range (1, 50).
+        An error is returned if ``max_num_results`` is given as an integer out
+        of the range (1, 50).
 
         The documentation at
         https://library.vuforia.com/articles/Solution/How-To-Perform-an-Image-Recognition-Query.  # noqa: E501
@@ -211,8 +212,67 @@ class TestMaxNumResults:
             status_code=codes.BAD_REQUEST,
         )
 
-    def test_invalid_type(self) -> None:
+    @pytest.mark.parametrize(
+		'num_results',
+		[b'0.1', b'1.1', b'a', b'2147483648'],
+	)
+    def test_invalid_type(
+        self,
+        high_quality_image: io.BytesIO,
+        vuforia_database_keys: VuforiaDatabaseKeys,
+        num_results: Union[bytes, float],
+    ) -> None:
         """
-        See https://github.com/adamtheturtle/vws-python/issues/357 for
-        implementing this test.
+        An error is returned if ``max_num_results`` is given as something other
+        than an integer.
+
+		Integers greater than 2147483647 are not considered integers because
+		they are bigger than Java's maximum integer.
         """
+        image_content = high_quality_image.read()
+        date = rfc_1123_date()
+        request_path = '/v1/query'
+        body = {
+            'image': ('image.jpeg', image_content, 'image/jpeg'),
+            'max_num_results': (None, num_results, 'text/plain'),
+        }
+        content, content_type_header = encode_multipart_formdata(body)
+        method = POST
+
+        access_key = vuforia_database_keys.client_access_key
+        secret_key = vuforia_database_keys.client_secret_key
+        authorization_string = authorization_header(
+            access_key=access_key,
+            secret_key=secret_key,
+            method=method,
+            content=content,
+            # Note that this is not the actual Content-Type header value sent.
+            content_type='multipart/form-data',
+            date=date,
+            request_path=request_path,
+        )
+
+        headers = {
+            'Authorization': authorization_string,
+            'Date': date,
+            'Content-Type': content_type_header,
+        }
+
+        response = requests.request(
+            method=method,
+            url=urljoin(base=VWQ_HOST, url=request_path),
+            headers=headers,
+            data=content,
+        )
+
+        expected_text = (
+            f"Invalid value '{num_results.decode()}' in form data part "
+            "'max_result'. "
+            'Expecting integer value in range from 1 to 50 (inclusive).'
+        )
+        assert response.text == expected_text
+        assert_vwq_failure(
+            response=response,
+            content_type='application/json',
+            status_code=codes.BAD_REQUEST,
+        )
