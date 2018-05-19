13f43ce7cc8f044eef3ec64c76259a0215967e4b
Adam Dangoor <adamdangoor@gmail.com>
2018-05-19 17:10:13 +0100

    Progress towards test for accept header

--- src/mock_vws/_mock_web_query_api.py
+++ src/mock_vws/_mock_web_query_api.py
@@ -111,6 +111,12 @@ class MockVuforiaWebQueryAPI:
         """
         Perform an image recognition query.
         """
+        if request.headers.get(
+            'Accept', 'application/json',
+        ) != 'application/json':
+            context.status_code = codes.NOT_ACCEPTABLE
+            return ''
+
         body_file = io.BytesIO(request.body)
 
         _, pdict = cgi.parse_header(request.headers['Content-Type'])
