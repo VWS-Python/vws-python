6051821b22a828de68cece74c746b4fc6d448d25
Adam Dangoor <adamdangoor@gmail.com>
2018-05-20 20:08:33 +0100

    Add test for missing image

--- tests/mock_vws/test_query.py
+++ tests/mock_vws/test_query.py
@@ -160,6 +160,58 @@ class TestSuccess:
 
 
 @pytest.mark.usefixtures('verify_mock_vuforia')
+class TestIncorrectFields:
+    """
+    Tests for unexpected fields.
+    """
+
+    def test_missing_image(
+        self,
+        high_quality_image: io.BytesIO,
+        vuforia_database_keys: VuforiaDatabaseKeys,
+    ) -> None:
+        image_content = high_quality_image.getvalue()
+        date = rfc_1123_date()
+        request_path = '/v1/query'
+        body = {}
+        content, content_type_header = encode_multipart_formdata(body)
+        method = POST
+
+        access_key = vuforia_database_keys.client_access_key
+        secret_key = vuforia_database_keys.client_secret_key
+        authorization_string = authorization_header(
+            access_key=access_key,
+            secret_key=secret_key,
+            method=method,
+            content=content,
+            # Note that this is not the actual Content-Type header value sent.
+            content_type='multipart/form-data',
+            date=date,
+            request_path=request_path,
+        )
+
+        headers = {
+            'Authorization': authorization_string,
+            'Date': date,
+            'Content-Type': content_type_header,
+        }
+
+        response = requests.request(
+            method=method,
+            url=urljoin(base=VWQ_HOST, url=request_path),
+            headers=headers,
+            data=content,
+        )
+
+        assert response.text == 'No image.'
+        assert_vwq_failure(
+            response=response,
+            status_code=codes.BAD_REQUEST,
+            content_type='application/json',
+        )
+
+
+@pytest.mark.usefixtures('verify_mock_vuforia')
 class TestMaxNumResults:
     """
     Tests for the ``max_num_results`` parameter.
