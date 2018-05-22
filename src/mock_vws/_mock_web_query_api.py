18d504f5c8f58530b306736bf26d050525945093
Adam Dangoor <adamdangoor@gmail.com>
2018-05-22 18:25:56 +0100

    Start of new validator

--- src/mock_vws/_mock_web_query_api.py
+++ src/mock_vws/_mock_web_query_api.py
@@ -31,6 +31,38 @@ ROUTES = set([])
 
 
 @wrapt.decorator
+def validate_content_type_header(
+    wrapped: Callable[..., str],
+    instance: Any,  # pylint: disable=unused-argument
+    args: Tuple[_RequestObjectProxy, _Context],
+    kwargs: Dict,
+) -> str:
+    """
+    Validate the accept header.
+
+    Args:
+        wrapped: An endpoint function for `requests_mock`.
+        instance: The class that the endpoint function is in.
+        args: The arguments given to the endpoint function.
+        kwargs: The keyword arguments given to the endpoint function.
+
+    Returns:
+        The result of calling the endpoint.
+        A `NOT_ACCEPTABLE` response if the Accept header is given and is not
+        'application/json' or '*/*'.
+    """
+    request, context = args
+
+    accept = request.headers.get('Accept')
+    if accept in ('application/json', '*/*', None):
+        return wrapped(*args, **kwargs)
+
+    context.headers.pop('Content-Type')
+    context.status_code = codes.NOT_ACCEPTABLE
+    return ''
+
+
+@wrapt.decorator
 def validate_accept_header(
     wrapped: Callable[..., str],
     instance: Any,  # pylint: disable=unused-argument
