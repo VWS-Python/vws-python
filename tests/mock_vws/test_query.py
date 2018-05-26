771a1855c5841c17d6824ad779d8020a611832e3
Adam Dangoor <adamdangoor@gmail.com>
2018-05-27 00:32:25 +0100

    Progress towards using helper

--- tests/mock_vws/test_query.py
+++ tests/mock_vws/test_query.py
@@ -1052,36 +1052,10 @@ class TestUpdate:
             vuforia_database_keys=vuforia_database_keys,
         )
 
-        date = rfc_1123_date()
-        request_path = '/v1/query'
         body = {'image': ('image.jpeg', image_content, 'image/jpeg')}
-        content, content_type_header = encode_multipart_formdata(body)
-        method = POST
-
-        access_key = vuforia_database_keys.client_access_key
-        secret_key = vuforia_database_keys.client_secret_key
-        authorization_string = authorization_header(
-            access_key=access_key,
-            secret_key=secret_key,
-            method=method,
-            content=content,
-            # Note that this is not the actual Content-Type header value sent.
-            content_type='multipart/form-data',
-            date=date,
-            request_path=request_path,
-        )
-
-        headers = {
-            'Authorization': authorization_string,
-            'Date': date,
-            'Content-Type': content_type_header,
-        }
-
-        response = requests.request(
-            method=method,
-            url=urljoin(base=VWQ_HOST, url=request_path),
-            headers=headers,
-            data=content,
+        response = query(
+            vuforia_database_keys=vuforia_database_keys,
+            body=body,
         )
 
         assert_query_success(response=response)
@@ -1100,3 +1074,11 @@ class TestUpdate:
         assert isinstance(target_timestamp, int)
         time_difference = abs(approximate_target_created - target_timestamp)
         assert time_difference < 5
+
+        body = {'image': ('image.jpeg', image_content, 'image/jpeg')}
+        response = query(
+            vuforia_database_keys=vuforia_database_keys,
+            body=body,
+        )
+        assert_query_success(response=response)
+        assert response.json()['results'] == []
