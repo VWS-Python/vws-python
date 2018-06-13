796932876ce1f574f4600cd9d9572a6aba14fe77
Adam Dangoor <adamdangoor@gmail.com>
2018-06-13 19:41:35 +0100

    Switch from constantly to Enum

--- src/mock_vws/_constants.py
+++ src/mock_vws/_constants.py
@@ -2,21 +2,30 @@
 Constants used to make the VWS mock.
 """
 
-from constantly import NamedConstant, Names, ValueConstant, Values
+from enum import Enum, auto
 
 
-class States(Names):  # type: ignore
+class States(Enum):
     """
     Constants representing various web service states.
     """
 
-    WORKING = NamedConstant()
+    WORKING = auto()
 
     # A project is inactive if the license key has been deleted.
-    PROJECT_INACTIVE = NamedConstant()
+    PROJECT_INACTIVE = auto()
+
+    def __repr__(self) -> str:
+        """
+        The representation should not include the generated number.
+        """
+        return '<{class_name}.{state_name}>'.format(
+            class_name=self.__class__.__name__,
+            state_name=self.name,
+        )
 
 
-class ResultCodes(Values):  # type: ignore
+class ResultCodes(Enum):
     """
     Constants representing various VWS result codes.
 
@@ -26,24 +35,24 @@ class ResultCodes(Values):  # type: ignore
     Some codes here are not documented in the above link.
     """
 
-    SUCCESS = ValueConstant('Success')
-    TARGET_CREATED = ValueConstant('TargetCreated')
-    AUTHENTICATION_FAILURE = ValueConstant('AuthenticationFailure')
-    REQUEST_TIME_TOO_SKEWED = ValueConstant('RequestTimeTooSkewed')
-    TARGET_NAME_EXIST = ValueConstant('TargetNameExist')
-    UNKNOWN_TARGET = ValueConstant('UnknownTarget')
-    BAD_IMAGE = ValueConstant('BadImage')
-    IMAGE_TOO_LARGE = ValueConstant('ImageTooLarge')
-    METADATA_TOO_LARGE = ValueConstant('MetadataTooLarge')
-    DATE_RANGE_ERROR = ValueConstant('DateRangeError')
-    FAIL = ValueConstant('Fail')
-    TARGET_STATUS_PROCESSING = ValueConstant('TargetStatusProcessing')
-    REQUEST_QUOTA_REACHED = ValueConstant('RequestQuotaReached')
-    TARGET_STATUS_NOT_SUCCESS = ValueConstant('TargetStatusNotSuccess')
-    PROJECT_INACTIVE = ValueConstant('ProjectInactive')
+    SUCCESS = 'Success'
+    TARGET_CREATED = 'TargetCreated'
+    AUTHENTICATION_FAILURE = 'AuthenticationFailure'
+    REQUEST_TIME_TOO_SKEWED = 'RequestTimeTooSkewed'
+    TARGET_NAME_EXIST = 'TargetNameExist'
+    UNKNOWN_TARGET = 'UnknownTarget'
+    BAD_IMAGE = 'BadImage'
+    IMAGE_TOO_LARGE = 'ImageTooLarge'
+    METADATA_TOO_LARGE = 'MetadataTooLarge'
+    DATE_RANGE_ERROR = 'DateRangeError'
+    FAIL = 'Fail'
+    TARGET_STATUS_PROCESSING = 'TargetStatusProcessing'
+    REQUEST_QUOTA_REACHED = 'RequestQuotaReached'
+    TARGET_STATUS_NOT_SUCCESS = 'TargetStatusNotSuccess'
+    PROJECT_INACTIVE = 'ProjectInactive'
 
 
-class TargetStatuses(Values):  # type: ignore
+class TargetStatuses(Enum):
     """
     Constants representing VWS target statuses.
 
@@ -51,6 +60,6 @@ class TargetStatuses(Values):  # type: ignore
     https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Retrieve-a-Target-Record
     """
 
-    PROCESSING = ValueConstant('processing')  # type: TargetStatuses
-    SUCCESS = ValueConstant('success')  # type: TargetStatuses
-    FAILED = ValueConstant('failed')  # type: TargetStatuses
+    PROCESSING = 'processing'
+    SUCCESS = 'success'
+    FAILED = 'failed'
