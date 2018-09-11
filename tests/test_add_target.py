3e417181b6415c1575bf506c420a20c0751a800a
Adam Dangoor <adamdangoor@gmail.com>
2018-09-11 11:15:09 +0100

    Add VWS Python mock

---
+++ tests/test_add_target.py
@@ -0,0 +1,27 @@
+from mock_vws import MockVWS
+
+@hypothesis?
+def test_add_target() -> None:
+    with MockVWS() as mock:
+        client = VWS(
+            server_access_key=mock.server_access_key,
+            server_secret_key=mock.server_secret_key,
+        )
+
+        target_id = client.add_target(
+            name='x',
+            width='x',
+            image='x',
+        )
+
+def test_authentication_error() -> None:
+    pass
+
+def test_clock_skew() -> None:
+    pass
+
+def test_width_invalid() -> None:
+    pass
+
+class TestTargetName:
+    pass
