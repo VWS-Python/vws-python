80cbfcc630f1c0f034834997fdfd0fe66ac23858
Adam Dangoor <adamdangoor@gmail.com>
2018-09-13 15:18:38 +0200

    Put tests into classes

--- tests/test_add_target.py
+++ tests/test_add_target.py
@@ -5,55 +5,79 @@ from mock_vws import MockVWS
 from requests import codes
 
 from vws import VWS
-from vws.exceptions import Fail
+from vws.exceptions import Fail, TargetNameExist
 
 
 # @hypothesis?
-def test_add_target(high_quality_image: io.BytesIO) -> None:
-    with MockVWS() as mock:
-        client = VWS(
-            server_access_key=mock.server_access_key,
-            server_secret_key=mock.server_secret_key,
-        )
 
-        client.add_target(
-            name='x',
-            width=1,
-            image=high_quality_image,
-        )
+class TestSuccess:
+    def test_add_target(self, high_quality_image: io.BytesIO) -> None:
+        with MockVWS() as mock:
+            client = VWS(
+                server_access_key=mock.server_access_key,
+                server_secret_key=mock.server_secret_key,
+            )
 
-def test_add_two_targets(high_quality_image: io.BytesIO) -> None:
-    with MockVWS() as mock:
-        client = VWS(
-            server_access_key=mock.server_access_key,
-            server_secret_key=mock.server_secret_key,
-        )
-
-        client.add_target(
-            name='x',
-            width=1,
-            image=high_quality_image,
-        )
-
-        client.add_target(
-            name='a',
-            width=1,
-            image=high_quality_image,
-        )
-
-def test_authentication_error(high_quality_image: io.BytesIO) -> None:
-    with MockVWS() as mock:
-        client = VWS(
-            server_access_key='a',
-            server_secret_key=mock.server_secret_key,
-        )
-
-        with pytest.raises(Fail) as exc:
-            target_id = client.add_target(
+            client.add_target(
                 name='x',
                 width=1,
                 image=high_quality_image,
             )
 
-        exception = exc.value
-        assert exception.response.status_code == codes.BAD_REQUEST
+    def test_add_two_targets(self, high_quality_image: io.BytesIO) -> None:
+        with MockVWS() as mock:
+            client = VWS(
+                server_access_key=mock.server_access_key,
+                server_secret_key=mock.server_secret_key,
+            )
+
+            client.add_target(
+                name='x',
+                width=1,
+                image=high_quality_image,
+            )
+
+            client.add_target(
+                name='a',
+                width=1,
+                image=high_quality_image,
+            )
+
+class TestName:
+    def test_add_two_targets_same_name(self, high_quality_image: io.BytesIO) -> None:
+        with MockVWS() as mock:
+            client = VWS(
+                server_access_key=mock.server_access_key,
+                server_secret_key=mock.server_secret_key,
+            )
+
+            client.add_target(
+                name='x',
+                width=1,
+                image=high_quality_image,
+            )
+
+            with pytest.raises(TargetNameExist) as exc:
+                client.add_target(
+                    name='x',
+                    width=1,
+                    image=high_quality_image,
+                )
+
+class TestAuthentication:
+    def test_authentication_error(self, high_quality_image: io.BytesIO) -> None:
+        with MockVWS() as mock:
+            client = VWS(
+                server_access_key='a',
+                server_secret_key=mock.server_secret_key,
+            )
+
+            with pytest.raises(Fail) as exc:
+                target_id = client.add_target(
+                    name='x',
+                    width=1,
+                    image=high_quality_image,
+                )
+
+            exception = exc.value
+            assert exception.response.status_code == codes.BAD_REQUEST
