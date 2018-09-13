6287ed73751a7140e1f5bf788f360a3fecd17f04
Adam Dangoor <adamdangoor@gmail.com>
2018-09-13 22:45:25 +0200

    Test for metadata

--- tests/test_add_target.py
+++ tests/test_add_target.py
@@ -10,59 +10,30 @@ from vws.exceptions import Fail, TargetNameExist
 
 # @hypothesis?
 
+@pytest.fixture()
+def client() -> VWS:
+    with MockVWS(real_http=False) as mock:
+        client = VWS(
+            server_access_key=mock.server_access_key,
+            server_secret_key=mock.server_secret_key,
+        )
+
+        yield client
+
 class TestSuccess:
-    def test_add_target(self, high_quality_image: io.BytesIO) -> None:
-        with MockVWS() as mock:
-            client = VWS(
-                server_access_key=mock.server_access_key,
-                server_secret_key=mock.server_secret_key,
-            )
+    def test_add_target(self, client: VWS, high_quality_image: io.BytesIO) -> None:
+        client.add_target(name='x', width=1, image=high_quality_image)
 
-            client.add_target(
-                name='x',
-                width=1,
-                image=high_quality_image,
-            )
-
-    def test_add_two_targets(self, high_quality_image: io.BytesIO) -> None:
-        with MockVWS() as mock:
-            client = VWS(
-                server_access_key=mock.server_access_key,
-                server_secret_key=mock.server_secret_key,
-            )
-
-            client.add_target(
-                name='x',
-                width=1,
-                image=high_quality_image,
-            )
-
-            client.add_target(
-                name='a',
-                width=1,
-                image=high_quality_image,
-            )
+    def test_add_two_targets(self, client: VWS, high_quality_image: io.BytesIO) -> None:
+        client.add_target(name='x', width=1, image=high_quality_image)
+        client.add_target(name='a', width=1, image=high_quality_image)
 
 class TestName:
-    def test_add_two_targets_same_name(self, high_quality_image: io.BytesIO) -> None:
-        with MockVWS() as mock:
-            client = VWS(
-                server_access_key=mock.server_access_key,
-                server_secret_key=mock.server_secret_key,
-            )
+    def test_add_two_targets_same_name(self, client: VWS, high_quality_image: io.BytesIO) -> None:
+        client.add_target(name='x', width=1, image=high_quality_image)
 
-            client.add_target(
-                name='x',
-                width=1,
-                image=high_quality_image,
-            )
-
-            with pytest.raises(TargetNameExist) as exc:
-                client.add_target(
-                    name='x',
-                    width=1,
-                    image=high_quality_image,
-                )
+        with pytest.raises(TargetNameExist) as exc:
+            client.add_target(name='x', width=1, image=high_quality_image)
 
 class TestAuthentication:
     def test_authentication_error(self, high_quality_image: io.BytesIO) -> None:
@@ -91,11 +62,26 @@ class TestImage:
 
 class TestapplicationMetadata:
 
-    def test_none(self):
-        pass
+    def test_none(self, client: VWS, high_quality_image: io.BytesIO):
+        client.add_target(
+            name='x',
+            width=1,
+            image=high_quality_image,
+            application_metadata=None,
+        )
 
-    def test_given(self):
-        pass
+    def test_given(self, client: VWS, high_quality_image: io.BytesIO):
+        client.add_target(
+            name='x',
+            width=1,
+            image=high_quality_image,
+            application_metadata='a',
+        )
 
-    def test_too_large(self):
-        pass
+    def test_too_large(self, client: VWS, high_quality_image: io.BytesIO):
+        client.add_target(
+            name='x',
+            width=1,
+            image=high_quality_image,
+            application_metadata='a' * 1024 * 1024,
+        )
