fca7cbd87f83d213c0d3bd21599b99d52a681acc
Adam Dangoor <adamdangoor@gmail.com>
2018-09-14 04:28:26 +0200

    Progress

--- tests/test_add_target.py
+++ tests/test_add_target.py
@@ -5,10 +5,47 @@ from mock_vws import MockVWS, States
 from requests import codes
 
 from vws import VWS
-from vws.exceptions import Fail, MetadataTooLarge, ProjectInactive, TargetNameExist
+from vws.exceptions import Fail, MetadataTooLarge, ProjectInactive, TargetNameExist, ImageTooLarge
+from PIL import Image
+import random
+import base64
 
 
-# @hypothesis?
+def make_image_file(
+    file_format: str,
+    color_space: str,
+    width: int,
+    height: int,
+) -> io.BytesIO:
+    """
+    Return an image file in the given format and color space.
+
+    The image file is filled with randomly colored pixels.
+
+    Args:
+        file_format: See
+            http://pillow.readthedocs.io/en/3.1.x/handbook/image-file-formats.html
+        color_space: One of "L", "RGB", or "CMYK". "L" means greyscale.
+        width: The width, in pixels of the image.
+        height: The width, in pixels of the image.
+
+    Returns:
+        An image file in the given format and color space.
+    """
+    image_buffer = io.BytesIO()
+    image = Image.new(color_space, (width, height))
+    pixels = image.load()
+    for i in range(height):
+        for j in range(width):
+            red = random.randint(0, 255)
+            green = random.randint(0, 255)
+            blue = random.randint(0, 255)
+            if color_space != 'L':
+                pixels[j, i] = (red, green, blue)
+    image.save(image_buffer, file_format)
+    image_buffer.seek(0)
+    return image_buffer
+
 
 @pytest.fixture()
 def client() -> VWS:
@@ -54,13 +91,38 @@ class TestAuthentication:
             assert exception.response.status_code == codes.BAD_REQUEST
 
 class TestImage:
-    def test_image_too_large(self):
-        pass
-
     def test_not_an_image(self):
         pass
 
-class TestapplicationMetadata:
+    def test_image_too_large(self, client: VWS):
+        max_bytes = 2.3 * 1024 * 1024
+        width = height = 886
+
+        width = width + 1
+        height = height + 1
+        png_too_large = make_image_file(
+            file_format='PNG',
+            color_space='RGB',
+            width=width,
+            height=height,
+        )
+
+        image_data = png_too_large.read()
+        image_data_encoded = base64.b64encode(image_data).decode('ascii')
+        image_content_size = len(image_data)
+        # We check that the image we created is just slightly smaller than the
+        # maximum file size.
+        #
+        # This is just because of the implementation details of
+        # ``max_image_file``.
+        assert image_content_size < max_bytes
+        assert (image_content_size * 1.05) > max_bytes
+
+        with pytest.raises(ImageTooLarge) as exc:
+            client.add_target(name='x', width=1, image=png_too_large)
+
+
+class TestApplicationMetadata:
 
     def test_none(self, client: VWS, high_quality_image: io.BytesIO):
         client.add_target(
