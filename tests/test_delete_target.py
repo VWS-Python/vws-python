36944d305ab32195e318617d2b87233d85d61a6b
Adam Dangoor <adamdangoor@gmail.com>
2018-09-14 08:34:09 +0200

    Progress

--- tests/test_delete_target.py
+++ tests/test_delete_target.py
@@ -47,6 +47,14 @@ class TestDelete:
         """
         No exception is raised when adding one target.
         """
+        target_id = client.add_target(
+            name='x',
+            width=1,
+            image=high_quality_image,
+        )
+
+        client.wait_for_target_processed(target_id=target_id)
+        client.delete_target(target_id=target_id)
 
     def test_delete_target_twice(
         self,
@@ -56,3 +64,12 @@ class TestDelete:
         """
         No exception is raised when adding one target.
         """
+        target_id = client.add_target(
+            name='x',
+            width=1,
+            image=high_quality_image,
+        )
+
+        client.wait_for_target_processed(target_id=target_id)
+        client.delete_target(target_id=target_id)
+        client.delete_target(target_id=target_id)
