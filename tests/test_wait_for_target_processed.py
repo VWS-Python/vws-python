da0076d061cdfae76300c9643bfcb27f6f513590
Adam Dangoor <adamdangoor@gmail.com>
2018-09-15 12:27:14 +0200

    Start of test for target processed

---
+++ tests/test_wait_for_target_processed.py
@@ -0,0 +1,42 @@
+"""
+Tests for helper function for waiting for a target to be processed.
+"""
+
+import io
+import random
+
+import pytest
+from mock_vws import MockVWS, States
+from PIL import Image
+from requests import codes
+
+from vws import VWS
+from vws.exceptions import (
+    BadImage,
+    Fail,
+    ImageTooLarge,
+    MetadataTooLarge,
+    ProjectInactive,
+    TargetNameExist,
+)
+
+
+class TestWaitForTargetProcessed:
+    """
+    Test for successfully adding a target.
+    """
+
+    def test_add_target(
+        self,
+        client: VWS,
+        high_quality_image: io.BytesIO,
+    ) -> None:
+        """
+        No exception is raised when adding one target.
+        """
+        client.add_target(name='x', width=1, image=high_quality_image)
+        target_record = client.get_target(target_id=target_id)
+        assert target_record['status'] == 'processing'
+        client.wait_for_target_processed(target_id=target_id)
+        target_record = client.get_target(target_id=target_id)
+        assert target_record['status'] != 'processing'
