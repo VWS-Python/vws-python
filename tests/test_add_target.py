6c578b67753e7a3fd646e5d91259b50c0b39bec6
Adam Dangoor <adamdangoor@gmail.com>
2018-09-15 18:43:53 +0200

    Add test for custom base URL

--- tests/test_add_target.py
+++ tests/test_add_target.py
@@ -4,6 +4,8 @@ Tests for helper function for adding a target to a Vuforia database.
 
 import io
 
+from mock_vws import MockVWS
+
 from vws import VWS
 
 
@@ -32,3 +34,28 @@ class TestSuccess:
         """
         client.add_target(name='x', width=1, image=high_quality_image)
         client.add_target(name='a', width=1, image=high_quality_image)
+
+
+class TestCustomBaseURL:
+    """
+    Tests for adding images to databases under custom VWS URLs.
+    """
+
+    def test_custom_base_url(self, high_quality_image: io.BytesIO) -> None:
+        """
+        It is possible to use add a target to a database under a custom VWS
+        URL.
+        """
+        base_vws_url = 'http://example.com'
+        with MockVWS(base_vws_url=base_vws_url) as mock:
+            client = VWS(
+                server_access_key=mock.server_access_key,
+                server_secret_key=mock.server_secret_key,
+                base_vws_url=base_vws_url,
+            )
+
+            client.add_target(
+                name='x',
+                width=1,
+                image=high_quality_image,
+            )
