2295d32d9f9dfcefc11879d8cb66cc9d4c5a28a9
Adam Dangoor <adamdangoor@gmail.com>
2018-09-17 10:39:28 +0200

    Progress

--- tests/test_unknown_target.py
+++ tests/test_unknown_target.py
@@ -3,29 +3,26 @@ Tests for passing invalid target IDs to helpers which require a target ID to
 be given.
 """
 
+from typing import Callable
+
 import pytest
+from _pytest.fixtures import SubRequest
 
 from vws import VWS
 from vws.exceptions import UnknownTarget
 
 
-class TestInvalidGivenID:
-    """
-    Tests for giving an invalid ID to helpers which require a target ID to be
-    given.
-    """
+@pytest.fixture()
+def _delete_target(client: VWS) -> Callable[[str], None]:
+    return client.delete_target
 
-    def test_invalid_given_id(self, client: VWS) -> None:
-        """
-        Giving an invalid ID to a helper which requires a target ID to be given
-        causes an ``UnknownTarget`` exception to be raised.
-        """
-        funcs = (
-            client.get_target_record,
-            client.get_target_summary_report,
-            client.delete_target,
-        )
 
-        for func in funcs:
-            with pytest.raises(UnknownTarget):
-                func(target_id='x')
+@pytest.mark.parametrize('fixture', ['_delete_target'])
+def test_invalid_given_id(fixture: Callable[[str], None], request: SubRequest,) -> None:
+    """
+    Giving an invalid ID to a helper which requires a target ID to be given
+    causes an ``UnknownTarget`` exception to be raised.
+    """
+    func = request.getfixturevalue(fixture)
+    with pytest.raises(UnknownTarget):
+        func(target_id='x')
