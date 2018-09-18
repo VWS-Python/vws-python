afd09bc606a6cb3a4fa91ebb2095fde56392a5c6
Adam Dangoor <adamdangoor@gmail.com>
2018-09-18 10:05:08 +0200

    Expand test

--- tests/test_unknown_target.py
+++ tests/test_unknown_target.py
@@ -17,8 +17,34 @@ def _delete_target(client: VWS) -> Callable[[str], None]:
     return client.delete_target
 
 
-@pytest.mark.parametrize('fixture', ['_delete_target'])
-def test_invalid_given_id(fixture: Callable[[str], None], request: SubRequest,) -> None:
+@pytest.fixture()
+def _get_target_record(client: VWS) -> Callable[[str], None]:
+    return client.get_target_record
+
+
+@pytest.fixture()
+def _wait_for_target_processed(client: VWS) -> Callable[[str], None]:
+    return client.wait_for_target_processed
+
+
+@pytest.fixture()
+def _get_target_summary_report(client: VWS) -> Callable[[str], None]:
+    return client.get_target_summary_report
+
+
+@pytest.mark.parametrize(
+    'fixture',
+    [
+        '_delete_target',
+        '_get_target_record',
+        '_wait_for_target_processed',
+        '_get_target_summary_report',
+    ],
+)
+def test_invalid_given_id(
+    fixture: Callable[[str], None],
+    request: SubRequest,
+) -> None:
     """
     Giving an invalid ID to a helper which requires a target ID to be given
     causes an ``UnknownTarget`` exception to be raised.
