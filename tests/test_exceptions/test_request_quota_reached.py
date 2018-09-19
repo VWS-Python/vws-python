8b73d347f16d663376c4e669838ce73bf6069f0d
Adam Dangoor <adamdangoor@gmail.com>
2018-09-19 19:49:03 +0200

    Add stub for test

---
+++ tests/test_exceptions/test_request_quota_reached.py
@@ -0,0 +1,9 @@
+"""
+Tests for hitting the 'RequestQuotaReached' exception.
+"""
+
+
+def test_request_quota_reached():
+    # See https://github.com/adamtheturtle/vws-python/issues/822 for writing
+    # this test.
+    pass
