94eff8b03feee83b6b743807c16cf900a64d36ea
Adam Dangoor <adamdangoor@gmail.com>
2018-09-20 18:08:31 +0200

    Start of dynamic exceptions

--- src/vws/exceptions.py
+++ src/vws/exceptions.py
@@ -4,77 +4,24 @@ Custom exceptions for Vuforia errors.
 
 from requests import Response
 
+names = [
+    'Success',
+    'TargetCreated',
+    'AuthenticationFailure',
+    'RequestTimeTooSkewed',
+    'TargetNameExist',
+    'UnknownTarget',
+    'BadImage',
+    'ImageTooLarge',
+    'MetadataTooLarge',
+    'DateRangeError',
+    'Fail',
+    'TargetStatusProcessing',
+    'RequestQuotaReached',
+    'TargetStatusNotSuccess',
+    'ProjectInactive',
+    'InactiveProject',
+]
 
-class UnknownTarget(Exception):
-    """
-    Exception raised when Vuforia returns a response with a result code
-    'UnknownTarget'.
-    """
-
-    def __init__(self, response: Response) -> None:
-        """
-        Args:
-            response: The response to a request to Vuforia.
-        """
-        super().__init__()
-        self.response = response
-
-
-class TargetStatusProcessing(Exception):
-    """
-    Exception raised when Vuforia returns a response with a result code
-    'TargetStatusProcessing'.
-    """
-
-    def __init__(self, response: Response) -> None:
-        """
-        Args:
-            response: The response to a request to Vuforia.
-        """
-        super().__init__()
-        self.response = response
-
-
-class MetadataTooLarge(Exception):
-    """
-    Exception raised when Vuforia returns a response with a result code
-    'MetadataTooLarge'.
-    """
-
-    def __init__(self, response: Response) -> None:
-        """
-        Args:
-            response: The response to a request to Vuforia.
-        """
-        super().__init__()
-        self.response = response
-
-
-class TargetNameExist(Exception):
-    """
-    Exception raised when Vuforia returns a response with a result code
-    'TargetNameExist'.
-    """
-
-    def __init__(self, response: Response) -> None:
-        """
-        Args:
-            response: The response to a request to Vuforia.
-        """
-        super().__init__()
-        self.response = response
-
-
-class ImageTooLarge(Exception):
-    """
-    Exception raised when Vuforia returns a response with a result code
-    'ImageTooLarge'.
-    """
-
-    def __init__(self, response: Response) -> None:
-        """
-        Args:
-            response: The response to a request to Vuforia.
-        """
-        super().__init__()
-        self.response = response
+for name in names:
+    globals()[name] = type(name, (Exception, ), {})
