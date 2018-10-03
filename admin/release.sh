8333e916c8d5d841e9ffe281f72316f73a517ee6
Adam Dangoor <adamdangoor@gmail.com>
2018-10-04 00:07:27 +0100

    fix name of dir

--- admin/release.sh
+++ admin/release.sh
@@ -6,7 +6,7 @@ set -ex
 # See the release process documentation for details.
 cd "$(mktemp -d)"
 git clone git@github.com:"${GITHUB_OWNER}"/vws-python.git
-cd vws-python-mock
+cd vws-python
 virtualenv -p python3 release
 source release/bin/activate
 pip install --editable .[dev]
