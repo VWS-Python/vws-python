d0de7f61ca05ac743a546cfa5e5c348cf64721c3
Adam Dangoor <adamdangoor@gmail.com>
2019-07-30 00:30:39 +0200

    Move exception test to exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -12,7 +12,7 @@ from mock_vws.states import States
 from PIL import Image
 from requests import codes
 
-from vws import VWS
+from vws import CloudRecoService, VWS
 from vws.exceptions import (
     AuthenticationFailure,
     BadImage,
@@ -227,6 +227,16 @@ def test_authentication_failure(high_quality_image: io.BytesIO) -> None:
                 width=1,
                 image=high_quality_image,
             )
+
+        assert exc.value.response.status_code == codes.UNAUTHORIZED
+
+        cloud_reco_client = CloudRecoService(
+            client_access_key=database.client_access_key,
+            client_secret_key='a',
+        )
+
+        with pytest.raises(AuthenticationFailure) as exc:
+            cloud_reco_client.query(image=high_quality_image)
 
         assert exc.value.response.status_code == codes.UNAUTHORIZED
 
--- tests/test_query.py
+++ tests/test_query.py
@@ -130,21 +130,6 @@ class TestMaxNumResults:
         )
         assert str(exc.value) == expected_value
 
-class TestBadCredentials:
-    """
-    Tests for using bad credentials.
-    """
-
-    def test_bad_credentials(self, high_quality_image: io.BytesIO):
-        cloud_reco_client = CloudRecoService(
-            client_access_key='a',
-            client_secret_key='a',
-        )
-        with pytest.raises(AuthenticationFailure) as exc:
-            cloud_reco_client.query(image=high_quality_image)
-
-        assert exc.value.response.status_code == codes.UNAUTHORIZED
-
 
 # TODO test custom base URL
 # TODO test options - include_target_data
