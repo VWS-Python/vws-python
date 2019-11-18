60f32c688bf521b4f61f4fa109b81a5dc9dbc4ae
Adam Dangoor <adamdangoor@gmail.com>
2019-11-18 23:12:00 +0000

    Progress towards usage documentation

--- src/vws/include_target_data.py
+++ src/vws/include_target_data.py
@@ -7,7 +7,8 @@ from enum import Enum
 
 class CloudRecoIncludeTargetData(Enum):
     """
-    Options for the ``include_target_data`` parameter of foo.
+    Options for the ``include_target_data`` parameter of
+    ``CloudRecoService.query``.
     """
 
     TOP = 'top'
