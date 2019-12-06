7a5fe7211b5749aa83e45419a48e5c5ea2d681fd
Adam Dangoor <adamdangoor@gmail.com>
2019-12-06 22:07:15 +0000

    Fix RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 Configuration for Sphinx.
 """
@@ -7,12 +6,11 @@ Configuration for Sphinx.
 # pylint: disable=invalid-name
 
 import datetime
-import os
-import sys
-from email import message_from_string
-from pathlib import Path
 
-import pkg_resources
+import vws
+
+project = 'VWS-Python'
+author = 'Adam Dangoor'
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -27,29 +25,13 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 
-docs_source_dir = Path(__file__).parent
-docs_dir = docs_source_dir.parent
-repo_dir = docs_dir.parent
-src_dir = repo_dir / 'src'
-distributions = {v.key: v for v in set(pkg_resources.working_set)}
-(distribution, ) = {
-    dist
-    for dist in distributions.values() if dist.location == str(src_dir)
-}
-project_name = distribution.project_name
-
-pkg_info = distribution.get_metadata('PKG-INFO')
-pkg_info_as_message = message_from_string(pkg_info)
-
-project = pkg_info_as_message['Name']
-author = pkg_info_as_message['Author']
 year = datetime.datetime.now().year
 copyright = f'{year}, {author}'  # pylint: disable=redefined-builtin
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
-version = distribution.version
+version = vws.__version__
 release = version.split('+')[0]
 
 substitutions = [
