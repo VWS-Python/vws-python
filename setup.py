8bf3e07ca81909bee012d48d601d02e42a93d0fa
Adam Dangoor <adamdangoor@gmail.com>
2019-12-07 13:39:21 +0000

    Move a bunch of options from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -14,34 +14,10 @@ with open('requirements.txt') as requirements:
 with open('dev-requirements.txt') as dev_requirements:
     DEV_REQUIRES = dev_requirements.readlines()
 
-with open('README.rst') as f:
-    LONG_DESCRIPTION = f.read()
-
 setup(
-    name='VWS Python',
     version=versioneer.get_version(),  # type: ignore
     cmdclass=versioneer.get_cmdclass(),  # type: ignore
-    author='Adam Dangoor',
-    author_email='adamdangoor@gmail.com',
-    description='Interact with the Vuforia Web Services (VWS) API.',
-    long_description=LONG_DESCRIPTION,
-    license='MIT',
-    packages=find_packages(where='src'),
-    zip_safe=False,
-    url='http://vws-python.readthedocs.io',
-    keywords='vuforia fake client',
-    package_dir={'': 'src'},
-    include_package_data=True,
     package_data={'vws': ['py.typed']},
     install_requires=INSTALL_REQUIRES,
-    extras_require={
-        'dev': DEV_REQUIRES,
-    },
-    classifiers=[
-        'Operating System :: POSIX',
-        'Environment :: Web Environment',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-        'Development Status :: 3 - Alpha',
-    ],
+    extras_require={'dev': DEV_REQUIRES},
 )
