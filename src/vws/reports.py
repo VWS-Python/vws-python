7ba59bdf855c6d34da62e13ac4ec4d12914c89cb
Adam Dangoor <adamdangoor@gmail.com>
2019-12-26 09:28:39 +0000

    Add new classes

--- src/vws/reports.py
+++ src/vws/reports.py
@@ -2,7 +2,9 @@
 Classes for representing Vuforia reports.
 """
 
+import datetime
 from dataclasses import dataclass
+from enum import Enum
 
 
 @dataclass
@@ -26,3 +28,36 @@ class DatabaseSummaryReport:
     request_usage: int
     target_quota: int
     total_recos: int
+
+
+class TargetStatuses(Enum):
+    """
+    Constants representing VWS target statuses.
+    See the 'status' field in
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API.html#How-To-Retrieve-a-Target-Record
+    """
+
+    PROCESSING = 'processing'
+    SUCCESS = 'success'
+    FAILED = 'failed'
+
+
+@dataclass
+class TargetSummaryReport:
+    """
+    A target summary report.
+
+    See
+    https://library.vuforia.com/articles/Solution/How-To-Use-the-Vuforia-Web-Services-API#How-To-Retrieve-a-Target-Summary-Report.
+    """
+
+    target_status: TargetStatuses
+    total_recos: int
+    database_name: str
+    target_name: str
+    upload_date: datetime.date
+    active_flag: bool
+    tracking_rating: int
+    total_recos: int
+    current_month_recos: int
+    previous_month_recos: int
