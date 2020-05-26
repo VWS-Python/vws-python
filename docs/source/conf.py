dd2d266533e33150291302c50f477d7c4366fca7
Adam Dangoor <adamdangoor@gmail.com>
2020-05-26 16:00:31 +0100

    Use new style for setting substitutions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,12 +38,6 @@ version = get_distribution(project).version
 _month, _day, _year, *_ = version.split('.')
 release = f'{_month}.{_day}.{_year}'
 
-substitutions = [
-    ('|release|', release),
-    ('|github-owner|', 'VWS-Python'),
-    ('|github-repository|', 'vws-python'),
-]
-
 language = None
 
 # The name of the syntax highlighting style to use.
@@ -115,6 +109,9 @@ autodoc_member_order = 'bysource'
 
 rst_prolog = f"""
 .. |project| replace:: {project}
+.. |release| replace:: {release}
+.. |github-owner| replace:: VWS-Python
+.. |github-repository| replace:: vws-python
 """
 
 always_document_param_types = True
