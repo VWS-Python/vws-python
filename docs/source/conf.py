9950215dae43f84fa67b41b1a1847275bfa10d68
Adam Dangoor <adamdangoor@gmail.com>
2021-08-04 00:06:19 +0100

    Use the `project_copyright` Sphinx variable rather than overriding the Python `copyright` builtin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 year = datetime.datetime.now().year
-copyright = f'{year}, {author}'  # pylint: disable=redefined-builtin
+project_copyright = f'{year}, {author}'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
