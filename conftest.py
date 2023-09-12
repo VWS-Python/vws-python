5969d4f670b29e9321a81d7deff2c9c857979877
Adam Dangoor <adamdangoor@gmail.com>
2023-09-12 10:13:07 +0100

    Use pyright strict mode

--- conftest.py
+++ conftest.py
@@ -2,14 +2,14 @@
 
 from doctest import ELLIPSIS
 
-from sybil import Sybil
-from sybil.parsers.rest import (
+from sybil import Sybil  # pyright: ignore[reportMissingTypeStubs]
+from sybil.parsers.rest import (  # pyright: ignore[reportMissingTypeStubs]
     ClearNamespaceParser,
     DocTestParser,
     PythonCodeBlockParser,
 )
 
-pytest_collect_file = Sybil(
+pytest_collect_file = Sybil(  # pyright: ignore[reportUnknownVariableType]
     parsers=[
         ClearNamespaceParser(),
         DocTestParser(optionflags=ELLIPSIS),
