312fa760ed399a00254beda6e18e59a1387e0494
Adam Dangoor <adamdangoor@gmail.com>
2024-01-23 09:37:39 +0000

    Fix double spacing

--- lint.mk
+++ lint.mk
@@ -54,11 +54,11 @@ linkcheck:
 
 .PHONY: pyproject-fmt
  pyproject-fmt:
-	pyproject-fmt --check  pyproject.toml
+	pyproject-fmt --check pyproject.toml
 
  .PHONY: fix-pyproject-fmt
  fix-pyproject-fmt:
-	pyproject-fmt  pyproject.toml
+	pyproject-fmt pyproject.toml
 
 .PHONY: spelling
 spelling:
