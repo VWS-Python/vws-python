f1189c89de014a4be26309f540036f5dce3297e6
Adam Dangoor <adamdangoor@gmail.com>
2024-08-29 13:53:56 +0100

    Run beartype on all tests

--- conftest.py
+++ conftest.py
@@ -2,12 +2,24 @@
 
 from doctest import ELLIPSIS
 
+import pytest
+from beartype import beartype
 from sybil import Sybil
 from sybil.parsers.rest import (
     ClearNamespaceParser,
     DocTestParser,
     PythonCodeBlockParser,
 )
+
+
+def pytest_collection_modifyitems(items: list[pytest.Item]) -> None:
+    """
+    Apply the beartype decorator to all collected test functions.
+    """
+    for item in items:
+        if isinstance(item, pytest.Function):
+            item.obj = beartype(obj=item.obj)
+
 
 pytest_collect_file = Sybil(
     parsers=[
