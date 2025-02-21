a42aa17bce274f17184da429dc3fce1d7956d41b
Adam Dangoor <adamdangoor@gmail.com>
2025-02-21 12:23:52 +0000

    Remove double setting of autoclass content - just use both

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,6 @@ html_theme_options = {
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "VWSPYTHONdoc"
-autoclass_content = "init"
 intersphinx_mapping = {
     "python": (f"https://docs.python.org/{minimum_python_version}", None),
 }
