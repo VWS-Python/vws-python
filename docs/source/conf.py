2ed9e31ab765e7f4fd0d9a0fa233ffcb8d606325
Adam Dangoor <adamdangoor@gmail.com>
2025-10-28 13:42:05 +0000

    Configure Furo top-of-page buttons with repository details

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,9 @@ html_show_sphinx = False
 html_show_sourcelink = False
 html_theme_options = {
     "sidebar_hide_name": False,
-    "top_of_page_buttons": ["view", "edit"],
+    "source_repository": "https://github.com/VWS-Python/vws-python/",
+    "source_branch": "main",
+    "source_directory": "docs/source/",
 }
 
 # Output file base name for HTML help builder.
